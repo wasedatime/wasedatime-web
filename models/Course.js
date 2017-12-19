8d05d74ec770537eaa90f9d10546ac9f247ba686
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-20 02:40:56 +0800

    Add Course Schema for mongoose

---
+++ models/Course.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+  title: String,
+  instructor: String,
+  year: String,
+  term: String,
+  school: String,
+  occurrences: [
+    {
+      day: Number,
+      start_period: String,
+      end_period: String,
+      start_time: Number,
+      end_time: Number,
+      building: String,
+      classroom: String,
+      location: String
+    }
+  ],
+  code: String
+});
+
+module.exports = mongoose.model('Course', courseSchema, 'courses_fund_eng_eng');

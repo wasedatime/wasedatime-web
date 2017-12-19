3da7e734e3c37773c6289b0b03c57596bdd5cbcb
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-19 23:10:10 +0800

    Create a simple express app

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);

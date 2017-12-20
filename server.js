598975669af74be20df5e13478332188929a8a99
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-20 15:18:34 +0800

    server.js: Import mongoose and connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/Course');
+require('./models/Classroom');
+require('./models/Building');
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(keys.mongoURI, {
+    useMongoClient: true
+  })
+  .then(() => {
+    console.log('App starting...');
+  })
+  .catch(err => {
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+  });
 
 const app = express();
 

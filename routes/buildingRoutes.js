01611b329c91caffee21b2ffcc7d83d30a19025e
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-20 15:19:20 +0800

    Create router for Building models

---
+++ routes/buildingRoutes.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Building = mongoose.model('Building');
+
+module.exports = app => {
+  app.get('/api/buildings', async (req, res) => {
+    const buildings = await Building.find({});
+    res.send(buildings);
+  });
+  app.get('/api/buildings/:buildingName', async (req, res) => {
+    const building = await Building.find({ name: req.params.buildingName });
+    res.send(building);
+  });
+};

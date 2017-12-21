a4a37220ecb3e5bf036296ec1c38ac17dae4024c
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-21 23:47:39 +0800

    read.test.js: Remove test case

--- test/mongoose/read.test.js
+++ test/mongoose/read.test.js
@@ -3,36 +3,8 @@ const Course = require('../../models/Course');
 const Classroom = require('../../models/Classroom');
 const mongoose = require('mongoose');
 
-describe('Reading courses', () => {
-  before(done => {
-    Course.find({ title: 'Calculus A' })
-      .then(courses => {
-        calculus_a = courses[0];
-        cls1 = calculus_a.occurrences[0].classroom;
-        cls2 = calculus_a.occurrences[1].classroom;
-        done();
-      })
-      .catch(err => {
-        console.log(err.stack);
-        assert(false);
-      });
-  });
-
-  it('finds the classroom of Calculus A', done => {
-    Classroom.find({ 'courses.title': 'Calculus A' })
-      .then(classrooms => {
-        if (cls1 !== classrooms[0].title) {
-          assert(cls1 == classrooms[1].title);
-          assert(cls2 == classrooms[0].title);
-        } else {
-          assert(cls1 == classrooms[0].title);
-          assert(cls2 == classrooms[1].title);
-        }
-        done();
-      })
-      .catch(err => {
-        console.log(err.stack);
-        assert(false);
-      });
+describe('Currently no test is created', () => {
+  it('pass', done => {
+    done();
   });
 });

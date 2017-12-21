a54d62f00af27f8bc2d32299c7021e6cde91e726
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-21 20:09:44 +0800

    buildingRoutes.js: Prevent returning any ids from get request

--- routes/buildingRoutes.js
+++ routes/buildingRoutes.js
@@ -4,11 +4,19 @@ const Building = mongoose.model('Building');
 
 module.exports = app => {
   app.get('/api/buildings', async (req, res) => {
-    const buildings = await Building.find({});
+    const buildings = await Building.find({}).select({
+      _id: 0,
+      'classrooms.id': 0
+    });
     res.send(buildings);
   });
   app.get('/api/buildings/:buildingName', async (req, res) => {
-    const building = await Building.find({ name: req.params.buildingName });
+    const building = await Building.find({
+      name: req.params.buildingName
+    }).select({
+      _id: 0,
+      'classrooms.id': 0
+    });
     res.send(building);
   });
 };

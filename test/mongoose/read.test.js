f1b226faf33a457863d66bcabcbb1b71cbb6f9a8
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-21 23:35:42 +0800

    read.test.js: Add sample test case

--- test/mongoose/read.test.js
+++ test/mongoose/read.test.js
@@ -1,21 +1,38 @@
 const assert = require('assert');
 const Course = require('../../models/Course');
+const Classroom = require('../../models/Classroom');
 const mongoose = require('mongoose');
 
 describe('Reading courses', () => {
-  let calculus_a = { _id: '5a321dcfb285813cff168c80' };
-  it('finds all courses with a title of Calculus A', done => {
-    Course.find({ title: 'Calculus A' }).then(courses => {
-      assert(courses[0]._id.toString() === calculus_a._id);
-      calculus_a = courses[0];
-      done();
-    });
+  before(done => {
+    Course.find({ title: 'Calculus A' })
+      .then(courses => {
+        calculus_a = courses[0];
+        cls1 = calculus_a.occurrences[0].classroom;
+        cls2 = calculus_a.occurrences[1].classroom;
+        done();
+      })
+      .catch(err => {
+        console.log(err.stack);
+        assert(false);
+      });
   });
 
-  it('find a course with a particular id', done => {
-    Course.findOne({ _id: calculus_a._id }).then(course => {
-      assert(course.title === calculus_a.title);
-      done();
-    });
+  it('finds the classroom of Calculus A', done => {
+    Classroom.find({ 'courses.title': 'Calculus A' })
+      .then(classrooms => {
+        if (cls1 !== classrooms[0].title) {
+          assert(cls1 == classrooms[1].title);
+          assert(cls2 == classrooms[0].title);
+        } else {
+          assert(cls1 == classrooms[0].title);
+          assert(cls2 == classrooms[1].title);
+        }
+        done();
+      })
+      .catch(err => {
+        console.log(err.stack);
+        assert(false);
+      });
   });
 });

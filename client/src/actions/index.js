9549c59d4ce804786882b5d6f4c04c46cedb96a1
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-24 21:57:12 +0800

    Hard code some action creators temporarily for testing

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,13 +9,38 @@ import {
 } from './types';
 
 export function fetchNishiBldgs() {
-  return { type: FETCH_NISHI_BLDGS };
+  var nishi_bldgs = [
+    '5a3b9b0fd7776b6d49e060b5', //51
+    '5a3b9b0fd7776b6d49e060b6', //52
+    '5a3b9b0fd7776b6d49e060b7' //53
+  ];
+  var nishi_bldgsById = {
+    '5a3b9b0fd7776b6d49e060b5': { name: '51' },
+    '5a3b9b0fd7776b6d49e060b6': { name: '52' },
+    '5a3b9b0fd7776b6d49e060b7': { name: '53' }
+  };
+  var payload = { nishi_bldgs, nishi_bldgsById };
+  return { type: FETCH_NISHI_BLDGS, payload };
 }
 
 export function fetchBldgClassrooms(bldg) {
-  return { type: FETCH_BLDG_CLASSROOMS, bldg };
+  var bldg_classrooms = [
+    '5a3b7c1ea2b3d21167f1564b',
+    '5a3b7c1ea2b3d21167f15679',
+    '5a3b7c1ea2b3d21167f15668'
+  ];
+  var bldg_classroomsById = {
+    '5a3b7c1ea2b3d21167f1564b': { name: '04-03B(社会文化領域研究室)' },
+    '5a3b7c1ea2b3d21167f15679': { name: '06-04' },
+    '5a3b7c1ea2b3d21167f15668': { name: '07-04' }
+  };
+  var payload = { bldg_classrooms, bldg_classroomsById };
+  return { type: FETCH_BLDG_CLASSROOMS, payload };
 }
 
 export function fetchBldgOccupiedClassrooms(bldg) {
-  return { type: FETCH_BLDG_OCCUPIED_CLASSROOMS, bldg };
+  var bldg_occupied_classrooms = ['5a3b7c1ea2b3d21167f1564b'];
+  var bldg_occupied_classroomsById = { name: '04-03B(社会文化領域研究室)' };
+  var payload = { bldg_occupied_classrooms, bldg_occupied_classroomsById };
+  return { type: FETCH_BLDG_OCCUPIED_CLASSROOMS, payload };
 }

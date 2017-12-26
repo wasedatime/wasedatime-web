e43c0051362cbc3fc8bcab77d68627d46ce0afa4
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-26 21:46:36 +0800

    Add background image and animation for home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
+import '../styles/home.css';
+
 const Home = () => {
   return (
-    <header>
-      <div className="wasetime__heading">
-        <h1 className="wasetime__title">WaseTime</h1>
-        <p className="wasetime__decription">
-          An unonfficial app for better syllabus searching, classroom finding,
-          and bus schedule checking at Waseda University.
-        </p>
+    <div className="home__wrapper">
+      <div className="home__overlay">
+        <header className="home__header">
+          <img className="home__logo" src="/img/logo.png" />
+          <h1 className="home__title">WaseTime</h1>
+          <p className="home__decription">
+            An unonfficial app for{' '}
+            <span className="home__description__highlight">
+              syllabus searching
+            </span>,{' '}
+            <span className="home__description__highlight">
+              classroom finding
+            </span>, and{' '}
+            <span className="home__description__highlight">
+              bus schedule checking
+            </span>{' '}
+            at Waseda University.
+          </p>
+        </header>
       </div>
-    </header>
+    </div>
   );
 };
 

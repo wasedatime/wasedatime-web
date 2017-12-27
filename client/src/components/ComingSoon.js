d8bb8ef22b444ae3fcc04a1bd0c878a8249d221a
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-28 01:41:49 +0800

    Create coming soon page for timetable and bus

---
+++ client/src/components/ComingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/comingsoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div className="comingsoon__wrapper">
+      <div className="comingsoon__overlay">
+        <div className="comingsoon">Coming Soon</div>
+      </div>
+    </div>
+  );
+};
+
+export default ComingSoon;

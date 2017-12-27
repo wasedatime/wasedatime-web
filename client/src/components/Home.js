4e334901a065dd56fd740a0cfcd87c81c3726a49
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-27 14:53:04 +0800

    Improve styles for Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,21 +7,25 @@ const Home = () => {
     <div className="home__wrapper">
       <div className="home__overlay">
         <header className="home__header">
-          <img className="home__logo" src="/img/logo.png" />
-          <h1 className="home__title">WaseTime</h1>
-          <p className="home__decription">
-            An unonfficial app for{' '}
-            <span className="home__description__highlight">
-              syllabus searching
-            </span>,{' '}
-            <span className="home__description__highlight">
-              classroom finding
-            </span>, and{' '}
-            <span className="home__description__highlight">
-              bus schedule checking
-            </span>{' '}
-            at Waseda University.
-          </p>
+          <div className="home__logo_wrapper">
+            <img className="home__logo" src="/img/logo.png" />
+          </div>
+          <div className="home__title__description__wrapper">
+            <h1 className="home__title">WaseTime</h1>
+            <p className="home__decription">
+              An unonfficial app for{' '}
+              <span className="home__description__highlight">
+                syllabus searching
+              </span>,{' '}
+              <span className="home__description__highlight">
+                classroom finding
+              </span>, and{' '}
+              <span className="home__description__highlight">
+                bus schedule checking
+              </span>{' '}
+              at Waseda University.
+            </p>
+          </div>
         </header>
       </div>
     </div>

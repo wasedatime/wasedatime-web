2daf0677dcaae09559bd04a56cf67b17d658a730
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-28 00:49:37 +0800

    Improve navbar and style for roomfinder

--- client/src/components/RoomFinderNavigation.js
+++ client/src/components/RoomFinderNavigation.js
@@ -4,25 +4,32 @@ import NavLink from './NavLink';
 
 const RoomFinderNavigation = () => {
   return (
-    <nav className="roomfinder__nav">
-      {/* <div className="comingsoon">Coming soon</div> */}
-      <NavLink
-        link="/roomfinder#mainCampus"
-        children="Main Campus"
-        className="roomfinder__nav__elem"
-      />
-      <NavLink
-        link="/roomfinder#nishiWasedaCampus"
-        children="Nishi Waseda Campus"
-        className="roomfinder__nav__elem"
-      />
-      {/* <div className="comingsoon">Coming soon</div> */}
-      <NavLink
-        link="/roomfinder#toyamaCampus"
-        children="Toyama Campus"
-        className="roomfinder__nav__elem"
-      />
-    </nav>
+    <div>
+      <nav className="roomfinder__nav">
+        <img
+          className="roomfinder__logo"
+          src="/img/campus.png"
+          alt="Campus logo"
+        />
+        <NavLink
+          link="/roomfinder#mainCampus"
+          children="Main"
+          className="roomfinder__nav__elem"
+        />
+        <NavLink
+          link="/roomfinder#nishiWasedaCampus"
+          children="Nishi"
+          className="roomfinder__nav__elem"
+        />
+        <NavLink
+          link="/roomfinder#toyamaCampus"
+          children="Toyama"
+          className="roomfinder__nav__elem"
+        />
+      </nav>
+      <div className="roomfinder__comingsoon__main">Coming Soon</div>
+      <div className="roomfinder__comingsoon__toyama">Coming Soon</div>
+    </div>
   );
 };
 

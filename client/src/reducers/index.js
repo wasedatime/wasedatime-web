df8b57204b359037eb81bcd36bace394c16e0828
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-29 17:26:53 +0800

    Remove unneeded actions and add one new

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,11 @@
 import {
+  SEARCH_COURSES,
   FETCH_NISHI_BLDGS,
-  FETCH_BLDG_CLASSROOMS,
-  FETCH_BLDG_OCCUPIED_CLASSROOMS,
   FETCH_BLDG_CURRENT_CLASSROOMS
 } from '../actions/types';
 
 const initialState = {
+  courseResults: [],
   nishiBldgIds: [],
   nishiBldgsById: [],
   bldgClassroomIds: [],
@@ -16,23 +16,16 @@ const initialState = {
 
 export default function app(state = initialState, action) {
   switch (action.type) {
+    case SEARCH_COURSES:
+      return {
+        ...state,
+        courseResults: action.payload.courseResults
+      };
     case FETCH_NISHI_BLDGS:
       return {
         ...state,
         nishiBldgIds: action.payload.nishiBldgIds,
         nishiBldgsById: action.payload.nishiBldgsById
-      };
-    case FETCH_BLDG_CLASSROOMS:
-      return {
-        ...state,
-        bldgClassroomIds: action.payload.bldgClassroomIds,
-        bldgClassroomsById: action.payload.bldgClassroomsById
-      };
-    case FETCH_BLDG_OCCUPIED_CLASSROOMS:
-      return {
-        ...state,
-        bldgOccupiedClassroomIds: action.payload.bldgOccupiedClassroomIds,
-        bldgOccupiedClassroomsById: action.payload.bldgOccupiedClassroomsById
       };
     case FETCH_BLDG_CURRENT_CLASSROOMS:
       return {

9bade0e92daf09d7e05611f742e36ec3dd94b080
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-31 00:27:37 +0800

    courseRoutes.js: Add error handling for invalid courseId types

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,9 +4,18 @@ const Course = mongoose.model('Course');
 
 module.exports = app => {
   app.get('/api/courses/:courseId', async (req, res) => {
-    const course = await Course.findOne({
-      _id: req.params.courseId
-    });
-    res.send(course);
+    try {
+      const course = await Course.findOne({
+        _id: req.params.courseId
+      });
+      res.send(course);
+    } catch (err) {
+      if (err.name === 'CastError') {
+        //Data was not found
+        res.status(200).send(null);
+      } else {
+        res.status(500).send('Error while fetching data from database');
+      }
+    }
   });
 };

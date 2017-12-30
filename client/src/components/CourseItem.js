b422d8ef466d25a33b5a037f109f95fe89ab60d2
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-12-30 18:04:46 +0800

    Add link and route for course items

--- client/src/components/CourseItem.js
+++ client/src/components/CourseItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const CourseItem = ({ course }) => {
   return (
     <li className="courseItem">
-      <div className="courseItem__title">{course.title}</div>
+      <Link className="courseItem__link" to={`/syllabus/${course._id.$oid}`}>
+        <div className="courseItem__title">{course.title}</div>
+      </Link>
       <div className="courseItem__instructor">{course.instructor}</div>
     </li>
   );
--- client/src/components/Syllabus.js
+++ client/src/components/Syllabus.js
@@ -3,17 +3,21 @@ import { Switch, Route } from 'react-router-dom';
 
 import CourseListContainer from '../containers/CourseListContainer';
 import SyllabusSearchBar from '../containers/SyllabusSearchBar';
+import CoursePageContainer from '../containers/CoursePageContainer';
 import '../styles/syllabus.css';
 
 const Syllabus = ({ match }) => {
   return (
     <div className="syllabus__wrapper">
       <SyllabusSearchBar />
-      <CourseListContainer />
-      {/* <SyllabusSearch />
       <Switch>
-        <Route component={CourseList} />
-      </Switch> */}
+        <Route exact path={`${match.url}`} component={CourseListContainer} />
+        <Route
+          path={`${match.url}/:courseId`}
+          component={CoursePageContainer}
+        />
+        <Route component={CourseListContainer} />
+      </Switch>
     </div>
   );
 };

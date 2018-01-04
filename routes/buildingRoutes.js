e860f4930c3ee06d33572d62c423171816147772
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-04 23:56:12 +0800

    Add basic error handling for routes

--- routes/buildingRoutes.js
+++ routes/buildingRoutes.js
@@ -5,22 +5,30 @@ const buildingList = require('../api/buildingList.js');
 
 module.exports = app => {
   app.get('/api/buildings', async (req, res) => {
-    const buildings = await Building.find({})
-      .sort({ name: 1 })
-      .select({
-        name: 1
-      });
-    res.send(buildings);
+    try {
+      const buildings = await Building.find({})
+        .sort({ name: 1 })
+        .select({
+          name: 1
+        });
+      res.send(buildings);
+    } catch (err) {
+      res.status(500).send('Error while fetching data from database');
+    }
   });
 
   app.get('/api/buildings/:buildingName', async (req, res) => {
-    if (buildingList.allBldgs.includes(req.params.buildingName)) {
-      const building = await Building.findOne({
-        name: req.params.buildingName
-      });
-      res.send(building);
-    } else {
-      res.status(404).send("Sorry can't find that!");
+    try {
+      if (buildingList.allBldgs.includes(req.params.buildingName)) {
+        const building = await Building.findOne({
+          name: req.params.buildingName
+        });
+        res.send(building);
+      } else {
+        res.status(404).send("Sorry can't find that!");
+      }
+    } catch (err) {
+      res.status(500).send('Error while fetching data from database');
     }
   });
 };

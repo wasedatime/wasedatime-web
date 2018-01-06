723971ae64301fc2b8cc828f3aa1e64e297ac547
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-06 20:32:40 +0800

    Copy buildingList.js and schema.js to actions directory

---
+++ client/src/actions/buildingList.js
@@ -0,0 +1,23 @@
+export const mainBldgs = [];
+
+export const nishiBldgs = [
+  '51',
+  '52',
+  '53',
+  '54',
+  '55',
+  '56',
+  '57',
+  '58',
+  '59',
+  '60',
+  '61',
+  '62',
+  '63'
+];
+
+export const toyamaBldgs = [];
+
+const allBldgs = mainBldgs.concat(nishiBldgs).concat(toyamaBldgs);
+
+export default allBldgs;
---
+++ client/src/actions/schema.js
@@ -0,0 +1,40 @@
+import { schema } from 'normalizr';
+
+const courseSchema = new schema.Entity('courses', {}, { idAttribute: '_id' });
+
+export const coursesSchema = [courseSchema];
+
+//{ _id: '5a3b9b0fd7776b6d49e060b5', name: '51' }
+const bldgSchema = new schema.Entity('bldgs', {}, { idAttribute: '_id' });
+
+//shorthand syntax for new schema.Array(bldgSchema)
+export const bldgListSchema = [bldgSchema];
+
+// { id: "5a3b7c1ea2b3d21167f156b1", name: "04-05" }
+const classroomSchema = new schema.Entity(
+  'classrooms',
+  {},
+  { idAttribute: 'id' }
+);
+
+export const classroomListSchema = [classroomSchema];
+
+// { _id: "5a3b7c1ea2b3d21167f15643",
+//   courses: {id: "5a3abb8f57ae267ac79fb856", title: "Communication Strategies 2 Mon-1 ISHIDA" },
+//   name : "B03" }
+const occupiedClassroomSchema = new schema.Entity(
+  'occupiedClassrooms',
+  {},
+  {
+    idAttribute: '_id',
+    processStrategy: entity => {
+      return {
+        id: entity._id,
+        name: entity.name,
+        courses: entity.courses
+      };
+    }
+  }
+);
+
+export const occupiedClassroomListSchema = [occupiedClassroomSchema];

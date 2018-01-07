d4d79d42cd9f76ac035623a7da9cfeb92cf32d0a
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-07 21:47:14 +0800

    Use axios request to fetch a single course instead of redux action

--- client/src/containers/CoursePageContainer.js
+++ client/src/containers/CoursePageContainer.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 
 import { fetchCourseById } from '../actions/index';
+import BlankCoursePage from '../components/BlankCoursePage';
 import CoursePage from '../components/CoursePage';
 
 class CoursePageContainer extends React.Component {
   constructor(props) {
     super(props);
     this.courseId = this.props.match.params.courseId;
+    this.state = {
+      isFetching: true,
+      coursePage: {}
+    };
   }
 
-  componentDidMount() {
-    this.props.fetchCourseById(this.courseId);
+  async componentDidMount() {
+    const res = await axios.get(`/api/courses/${this.courseId}`);
+    this.setState({
+      isFetching: false,
+      coursePage: res.data
+    });
   }
 
   render() {
-    if (this.props.coursePage._id) {
-      return <CoursePage coursePage={this.props.coursePage} />;
+    if (this.state.isFetching) {
+      return <BlankCoursePage />;
+    }
+
+    if (this.state.coursePage._id) {
+      return <CoursePage coursePage={this.state.coursePage} />;
     } else {
       return <Redirect from={this.props.match.url} to="/syllabus" />;
     }
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const coursePage = state.coursePage;
-  return { coursePage };
-};
-
-const mapDispatchToProps = {
-  fetchCourseById
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(
-  CoursePageContainer
-);
+export default CoursePageContainer;
 
 CoursePageContainer.propTypes = {
-  match: PropTypes.object.isRequired,
-  coursePage: PropTypes.object.isRequired,
-  fetchCourseById: PropTypes.func.isRequired
+  match: PropTypes.object.isRequired
 };

625e51c99a5c471f84749691c0ace3090fac8f4c
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-08 16:49:30 +0800

    Move buildingList.js from api to data directory

---
+++ data/buildingList.js
@@ -0,0 +1,23 @@
+const mainBuildings = [];
+
+const nishiBuildings = [
+  '51',
+  '52',
+  '53',
+  '54',
+  '55',
+  '56',
+  '57',
+  '58',
+  '59',
+  '60',
+  '61',
+  '62',
+  '63'
+];
+
+const toyamaBuildings = [];
+
+module.exports.allBuildings = mainBuildings
+  .concat(nishiBuildings)
+  .concat(toyamaBuildings);

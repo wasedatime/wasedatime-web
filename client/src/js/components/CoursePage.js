7a655c54642f9b8818fcf1331ac9c04196372be9
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-09 00:18:24 +0800

    CoursePage.js: Improve styling and props processing

--- client/src/js/components/CoursePage.js
+++ client/src/js/components/CoursePage.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { media } from '../utils/styledComponents';
 import PropTypes from 'prop-types';
-
-const getLocation = (building, classroom) => {
-  if (building === '-1') {
-    return `others-${classroom}`;
-  } else {
-    return `${building}-${classroom}`;
-  }
-};
-
-const getKey = (day, start_period, location) => {
-  return `${day}-${start_period}-${location}`;
-};
 
 const Wrapper = styled('div')`
   display: flex;
@@ -25,28 +14,105 @@ const Overlay = styled('div')`
   display: flex;
   flex-direction: column;
   flex: 1 0 auto;
-  align-items: center;
-  padding: 25px;
+  padding: 25px 20vh;
   background-color: rgba(0, 0, 0, 0.2);
+  font-size: 1.8rem;
+  ${media.desktop`padding: 25px 20vh;;`};
+  ${media.tablet`padding: 25px 10vh;;`};
+  ${media.phone`padding: 25px ;`};
 `;
 
+const StyledHeader = styled('header')`
+  padding-bottom: 11px;
+  border-bottom: 1px solid #777;
+`;
+
+const Title = styled('h2')`
+  margin: 8px auto;
+`;
+
+const YearTerm = styled('div')`
+  font-size: 2rem;
+`;
+
+const Instructor = styled('div')`
+  margin-top: 8px;
+`;
+
+const DetailWrapper = styled('div')`
+  margin: 8px 0;
+`;
+
+const StyledHeading = styled('h4')`
+  margin: 5px auto;
+`;
+
+const OccurrenceList = styled('ul')`
+  list-style: none;
+  margin: 0 auto;
+  padding: 0px;
+`;
+
+const StyledLink = styled(Link)`
+  align-self: center;
+`;
 const StyledButton = styled('button')`
   background-color: #fff;
   padding: 12px;
   margin: 10px 0;
   width: 160px;
   text-align: center;
-  text-decoration: none;
   border-radius: 8px;
   border-bottom: none;
   box-shadow: 0 5px #999;
   font-size: 1.7rem;
 `;
 
-const CourseDetailWrapper = styled('div')`
-  display: flex;
-  flex-direction: column;
+const LinkList = styled('ul')`
+  padding: 0px 25px;
+  margin: 5px auto;
 `;
+
+const getDay = day => {
+  switch (day) {
+    case 1:
+      return 'Mon.';
+    case 2:
+      return 'Tue.';
+    case 3:
+      return 'Wed.';
+    case 4:
+      return 'Thur.';
+    case 5:
+      return 'Fri.';
+    case 6:
+      return 'Sat.';
+    case 7:
+      return 'Sun.';
+    default:
+      return '';
+  }
+};
+
+const getLocation = (building, classroom) => {
+  if (building === '-1') {
+    return classroom;
+  } else {
+    return `${building}-${classroom}`;
+  }
+};
+
+const getPeriod = (start_period, end_period) => {
+  if (start_period === end_period) {
+    return start_period;
+  } else {
+    return `${start_period}-${end_period}`;
+  }
+};
+
+const getKey = (day, period, location) => {
+  return `${day}-${period}-${location}`;
+};
 
 const CoursePage = ({ coursePage }) => {
   const links = coursePage.links.map(link => {
@@ -58,39 +124,37 @@ const CoursePage = ({ coursePage }) => {
   });
 
   const occurrences = coursePage.occurrences.map(occurrence => {
+    const day = getDay(occurrence.day);
+    const period = getPeriod(occurrence.start_period, occurrence.end_period);
     const location = getLocation(occurrence.building, occurrence.classroom);
-    const key = getKey(occurrence.day, occurrence.start_period, location);
+    const key = getKey(day, period, location);
     return (
       <li key={key}>
-        <div>{`Day: ${occurrence.day}`}</div>
-        <div>{`Time: ${occurrence.start_period} to ${
-          occurrence.end_period
-        } period`}</div>
-        <div>{`Location: ${location}`}</div>
+        <div>{`${day}${period} • ${location}`}</div>
       </li>
     );
   });
   return (
     <Wrapper>
       <Overlay>
-        <CourseDetailWrapper>
-          <h2>{coursePage.title}</h2>
-          <div>{`Code: ${coursePage.code}`}</div>
-          <div>{`Instructor: ${coursePage.instructor}`}</div>
-          <div>{`Year: ${coursePage.year}`}</div>
-          <div>{`Term: ${coursePage.term}`}</div>
+        <StyledHeader>
+          <Title>{coursePage.title}</Title>
+          <YearTerm>{`${coursePage.year} ${coursePage.term}`}</YearTerm>
+          <Instructor>{coursePage.instructor}</Instructor>
+        </StyledHeader>
+        <DetailWrapper>
+          <StyledHeading>Time • Location</StyledHeading>
+          <OccurrenceList>{occurrences}</OccurrenceList>
+          <StyledHeading>Code</StyledHeading>
+          <div>{coursePage.code}</div>
           <div>
-            <div>Occurrences: </div>
-            <ul>{occurrences}</ul>
+            <StyledHeading>Official Syllabus Links:</StyledHeading>
+            <LinkList>{links}</LinkList>
           </div>
-          <div>
-            <div>Official Syllabus Link(s):</div>
-            <ul>{links}</ul>
-          </div>
-        </CourseDetailWrapper>
-        <Link to="/syllabus">
+        </DetailWrapper>
+        <StyledLink to="/syllabus">
           <StyledButton>Return to search</StyledButton>
-        </Link>
+        </StyledLink>
       </Overlay>
     </Wrapper>
   );
@@ -101,4 +165,3 @@ export default CoursePage;
 CoursePage.propTypes = {
   coursePage: PropTypes.object.isRequired
 };
-

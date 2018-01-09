79be0150b858992dc4109f8c414ffd9119287231
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-10 02:32:18 +0800

    Install react-ga and add basic pageview analytics

--- client/src/js/components/Root.js
+++ client/src/js/components/Root.js
@@ -1,15 +1,49 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+
+ReactGA.initialize('UA-112185819-1', { debug: true });
+
+// A invisible component that sends a GA pageview every time
+// the pathname of the user is changed.
+// Written by @dandalf and improved by @ianarundale. I removed
+// props.location.search since query string has not been implemented.
+// Reference: https://github.com/react-ga/react-ga/issues/122
+class Analytics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sendPageChange(props.location.pathname);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      this.sendPageChange(nextProps.location.pathname);
+    }
+  }
+
+  sendPageChange(pathname) {
+    const page = pathname;
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
+  }
+
+  render() {
+    return null;
+  }
+}
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <div>
+          <Route path="/" component={Analytics} />
+          <App />
+        </div>
       </BrowserRouter>
     </Provider>
   );

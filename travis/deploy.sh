e31f506fcadee237d76f6a014e1ddfb7e01da609
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 21:48:30 +0800

    Investigate why git is not pushing the added files

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,9 +4,10 @@ set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     git remote add deploy 'deploy@139.59.122.225:wasetime-web.git'
     git add .
-    git add client/build --force
+    git add client/build/* --force
+    git status
     git commit -m "Deploy from Travis CI"
-    git push deploy master
+    git push deploy master --force
 else
     echo "Not deploying, since this branch isn't master."
 fi

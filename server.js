02dfddb70ee5b72e739ccb32aae3252065197d22
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 20:35:11 +0800

    Add custom path for .env during production

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '/var/www/.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');

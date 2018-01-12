c28b90fdbfcf203ef46ca16be3da01dd95098c44
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 18:03:48 +0800

    Try using ip address for ssh instead of domain name

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-
-    git remote add deploy ssh://deploy@wasetime.com/wasetime-web.git
+    git remote add deploy ssh://deploy@139.59.122.225/wasetime-web.git
     git add .
     git commit -m "Deploy from Travis CI"
     git push deploy master

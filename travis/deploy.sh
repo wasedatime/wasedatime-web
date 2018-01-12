70e67f04e1ec86ad2b7b44e626792882a7dac5b6
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 20:46:21 +0800

    Remove redundant code

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -x
 
-#remove .ssh directory before deploying
-rm -R .ssh
-
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     git remote add deploy 'deploy@139.59.122.225:wasetime-web.git'
+    git add .
     git add client/build --force
-    git status
     git commit -m "Deploy from Travis CI"
     git push deploy master
 else

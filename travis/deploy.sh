f1a041801664200501622510b6ad7e72d5fdca26
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 19:04:41 +0800

    deploy.sh: Examine git status and disable deployment temporarily

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-    git remote add deploy ssh://deploy@139.59.122.225/wasetime-web.git
+    git remote add deploy 'deploy@139.59.122.225:wasetime-web.git'
+    git status
     git add .
-    git commit -m "Deploy from Travis CI"
-    git push deploy master
+    git status
+    #git commit -m "Deploy from Travis CI"
+    #git push deploy master
 else
     echo "Not deploying, since this branch isn't master."
 fi

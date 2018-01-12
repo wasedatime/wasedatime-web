62b08ae235f9310bcf6e166ae7dfb12332a33161
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 17:46:35 +0800

    Fix incorrect syntax again

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -4,8 +4,7 @@
 set -x
 
 #Import private deploy key
-openssl aes-256-cbc -K $encrypted_0bf5282a1b59_key -iv $encrypted_0bf5282a1b59_iv
-  -in deploy_rsa.enc -out deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_0bf5282a1b59_key -iv $encrypted_0bf5282a1b59_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc
 chmod 600 deploy_rsa
 mv deploy_rsa ~/.ssh/id_rsa

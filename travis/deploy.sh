38071e5beab3b58cab3f590fbe3d0d09754a93b8
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 19:22:12 +0800

    Force git to add  production files before deployment

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,7 +4,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     git remote add deploy 'deploy@139.59.122.225:wasetime-web.git'
     git status
     git add .
-    git status
+    git add client/build --force
+    git status --ignored
     #git commit -m "Deploy from Travis CI"
     #git push deploy master
 else

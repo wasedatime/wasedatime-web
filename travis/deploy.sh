75c17b4a534d26e142ca1afd87a7f3566487a8b1
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 22:08:28 +0800

    Fix git HEAD detached when adding

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,11 +2,14 @@
 set -x
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+    #Fix HEAD detached
+    git checkout master
     git remote add deploy 'deploy@139.59.122.225:wasetime-web.git'
     git add .
     git add client/build/* --force
     git status
     git commit -m "Deploy from Travis CI"
+    git status
     git push deploy master --force
 else
     echo "Not deploying, since this branch isn't master."

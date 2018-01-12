4bcd4ad9ba2e3961d847ae2e2113e312382f6e47
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-01-12 13:13:21 +0800

    Remove mongoose test cases

--- test/no.test.js
+++ test/no.test.js
@@ -1,7 +1,4 @@
 const assert = require('assert');
-const Course = require('../../models/Course');
-const Classroom = require('../../models/Classroom');
-const mongoose = require('mongoose');
 
 describe('Currently no test is created', () => {
   Let it fail

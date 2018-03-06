306f17cdc8f87640f4b3add5aebfbfd882170614
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-06 20:38:33 +0800

    Navigation.js: Highlight nav icon according to user url

--- client/src/js/components/Navigation.js
+++ client/src/js/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router'
 import styled from 'styled-components';
 
 const StyledNav = styled('nav')`
@@ -12,30 +13,46 @@ const StyledNav = styled('nav')`
 
 const StyledLink = styled(Link)`
   padding: 2rem 1.5rem;
-  color: rgba(255, 255, 255, 1);
+  ${'' /* color: #ffffff; */}
   text-align: center;
   text-decoration: none;
   font-size: 1.7rem;
   font-weight: 330;
 `;
 
-const Navigation = () => {
+const Navigation = (props) => {
+  var pathname = props.location.pathname;
+  var navItems = [{
+    "path": "/timetable",
+    "name": "calendar-alt"
+  },{
+    "path": "/syllabus",
+    "name": "book"
+  },{
+    "path": "/roomfinder",
+    "name": "building"
+  },{
+    "path": "/bus",
+    "name": "bus"
+  }];
+  var styledLinks = navItems.map((nav) => {
+    var fontName = nav['name'];
+    // TODO see if inline style textShadow: "0px 0px 5px white" works :'(
+    var fontStyle = pathname.includes(nav['path'])
+      ? {color: "#b51e36"} : {color: "#ffffff"};
+    var font = <i className={`fas fa-${fontName} fa-2x`}
+                data-fa-transform="shrink-2" ></i>
+    return (
+      <StyledLink to={nav['path']} key={fontName} style={fontStyle}>
+        {font}
+      </StyledLink>
+    );
+  });
   return (
     <StyledNav>
-      <StyledLink to="/timetable">
-        <i className="fas fa-calendar-alt fa-2x" data-fa-transform="shrink-2"></i>
-      </StyledLink>
-      <StyledLink to="/syllabus">
-        <i className="fas fa-book fa-2x" data-fa-transform="shrink-2"></i>
-      </StyledLink>
-      <StyledLink to="/roomfinder">
-        <i className="fas fa-building fa-2x" data-fa-transform="shrink-2"></i>
-      </StyledLink>
-      <StyledLink to="/bus">
-        <i className="fas fa-bus fa-2x" data-fa-transform="shrink-2"></i>
-      </StyledLink>
+      {styledLinks}
     </StyledNav>
   );
 };
 
-export default Navigation;
+export default withRouter(Navigation);

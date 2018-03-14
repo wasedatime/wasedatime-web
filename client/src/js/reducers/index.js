8bc41e141e8aa4f89a028450e84d6bb25a167c47
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-14 21:10:28 +0800

    Add stats reducer

--- client/src/js/reducers/index.js
+++ client/src/js/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 
+import stats from './stats';
 import searchTerm from './searchTerm';
 import courses from './courses';
 import buildings from './buildings';
 
-const app = combineReducers({ searchTerm, courses, buildings });
+const app = combineReducers({ stats, searchTerm, courses, buildings});
 
 export default app;
---
+++ client/src/js/reducers/stats.js
@@ -0,0 +1,51 @@
+import { combineReducers } from 'redux';
+
+import {
+  FETCH_STATS_REQUEST,
+  FETCH_STATS_SUCCESS,
+  FETCH_STATS_FAILURE
+} from '../actions/types';
+
+//TODO Think about a way of abstracting list and stats into one reusable reducer.
+//TODO Perhaps combine is Fetching, error into one reducer called status?
+
+const info = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STATS_SUCCESS:
+      return action.response;
+    default:
+      return state;
+  }
+};
+
+const isFetching = (state = false, action) => {
+  switch (action.type) {
+    case FETCH_STATS_REQUEST:
+      return true;
+    case FETCH_STATS_SUCCESS:
+    case FETCH_STATS_FAILURE:
+      return false;
+    default:
+      return state;
+  }
+};
+
+const error = (state = null, action) => {
+  switch (action.type) {
+    case FETCH_STATS_FAILURE:
+      return action.error;
+    case FETCH_STATS_REQUEST:
+    case FETCH_STATS_SUCCESS:
+      return null;
+    default:
+      return state;
+  }
+};
+
+const stats =  combineReducers({ info, isFetching, error });
+
+export default stats;
+
+export const getInfo = state => state.info;
+export const getIsFetching = state => state.isFetching;
+export const getError = state => state.error;

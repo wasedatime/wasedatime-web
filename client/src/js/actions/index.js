d85f9d8d3005e64974bd966c0b5c0ea5bc448a8a
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-14 09:10:56 +0800

    Create redux actions for fetching stats

--- client/src/js/actions/index.js
+++ client/src/js/actions/index.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { normalize } from 'normalizr';
 
 import {
+  FETCH_STATS_REQUEST,
+  FETCH_STATS_SUCCESS,
+  FETCH_STATS_FAILURE,
   FETCH_COURSES_REQUEST,
   FETCH_COURSES_SUCCESS,
   FETCH_COURSES_FAILURE,
@@ -11,6 +14,26 @@ import {
   SEARCH_COURSES
 } from './types';
 import * as schema from '../data/schema';
+
+export const fetchStats = () => async (dispatch, getState) => {
+  dispatch({
+    type: FETCH_STATS_REQUEST
+  });
+
+  try {
+    const res = await axios.get('/api/stats');
+    const stats = res.data;
+    dispatch({
+      type: FETCH_STATS_SUCCESS,
+      response: stats
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_STATS_FAILURE,
+      response: error.response
+    });
+  }
+}
 
 export const fetchCourses = filter => async (dispatch, getState) => {
   dispatch({

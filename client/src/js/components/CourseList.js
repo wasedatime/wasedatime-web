df5fcc010250caa8a8d4916c3951c9044559d2fe
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-03-18 17:53:41 +0800

    CourseList.js: Remove unneeded if condition, add filter icon

--- client/src/js/components/CourseList.js
+++ client/src/js/components/CourseList.js
@@ -9,21 +9,26 @@ import {
 import PropTypes from 'prop-types';
 
 import CourseItem from './CourseItem';
+import { media } from '../utils/styledComponents';
 import { Overlay } from '../styled-components/Overlay';
 import { Wrapper } from '../styled-components/Wrapper';
 
 const ExtendedOverlay = Overlay.extend`
-  padding: 36px 25px 25px 25px;
+  padding: 36px 12.5vw 0px 12.5vw;
+  ${media.phone`padding: 36px 7.5vw 0px 7.5vw;`};
 `;
 
 const ListWrapper = styled('div')`
   flex: 1 0 auto;
 `;
 
-const Summary = styled('div')`
-  margin: 10px 0;
-  align-self: flex-end;
-`;
+const Menu = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 0px;
+`
 
 const cache = new CellMeasurerCache({
   fixedWidth: true,
@@ -31,66 +36,63 @@ const cache = new CellMeasurerCache({
 });
 
 const CourseList = ({ searchTerm, searchResults }) => {
-  if (searchResults.length !== 0) {
-    cache.clearAll();
-    const rowRenderer = ({
-      index,
-      isScrolling,
-      isVisible,
-      key,
-      parent,
-      style
-    }) => {
-      return (
-        <CellMeasurer
-          cache={cache}
-          columnIndex={0}
+  cache.clearAll();
+  const rowRenderer = ({
+    index,
+    isScrolling,
+    isVisible,
+    key,
+    parent,
+    style
+  }) => {
+    return (
+      <CellMeasurer
+        cache={cache}
+        columnIndex={0}
+        key={key}
+        parent={parent}
+        rowIndex={index}
+      >
+        <CourseItem
           key={key}
-          parent={parent}
-          rowIndex={index}
-        >
-          <CourseItem
-            key={key}
-            style={style}
-            searchTerm={searchTerm}
-            course={searchResults[index]}
-          />
-        </CellMeasurer>
-      );
-    };
-    const resultsCount = searchResults.length;
-    return (
-      <Wrapper>
-        <ExtendedOverlay>
-          <Summary>{`${resultsCount} results`}</Summary>
-          <ListWrapper>
-            <AutoSizer>
-              {({ width, height }) => {
-                return (
-                  <List
-                    width={width}
-                    height={height}
-                    rowCount={resultsCount}
-                    deferredMeasurementCache={cache}
-                    rowHeight={cache.rowHeight}
-                    rowRenderer={rowRenderer}
-                  />
-                );
-              }}
-            </AutoSizer>
-          </ListWrapper>
-        </ExtendedOverlay>
-      </Wrapper>
+          style={style}
+          searchTerm={searchTerm}
+          course={searchResults[index]}
+        />
+      </CellMeasurer>
     );
-  } else {
-    return (
-      <Wrapper>
-        <ExtendedOverlay>
-          <Summary>0 results</Summary>
-        </ExtendedOverlay>
-      </Wrapper>
-    );
-  }
+  };
+  const resultsCount = searchResults.length;
+  return (
+    <Wrapper>
+      <ExtendedOverlay>
+        <Menu>
+          <span>{`${resultsCount} results`}</span>
+          <a>
+            <span>
+              <i className="fa fa-filter fa-lg" ></i> Filter
+            </span>
+          </a>
+        </Menu>
+        <ListWrapper>
+          <AutoSizer>
+            {({ width, height }) => {
+              return (
+                <List
+                  width={width}
+                  height={height}
+                  rowCount={resultsCount}
+                  deferredMeasurementCache={cache}
+                  rowHeight={cache.rowHeight}
+                  rowRenderer={rowRenderer}
+                />
+              );
+            }}
+          </AutoSizer>
+        </ListWrapper>
+      </ExtendedOverlay>
+    </Wrapper>
+  );
 };
 
 export default CourseList;

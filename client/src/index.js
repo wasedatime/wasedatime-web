5fba19d551dcda2263cf43a951918e9f0bd7b804
nd-02110114 <nd.12021218@gmail.com>
2018-05-08 00:24:49 +0900

    :sparkles: (client) create SW import function for index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom';
 
 import configureStore from './configureStore';
 import Root from './js/components/Root';
+import registerServiceWorker from './registerServiceWorker'
 
 import 'normalize-css/normalize.css';
 import './styles/styles.css';
 
 const store = configureStore();
+registerServiceWorker()
 
 render(<Root store={store} />, document.getElementById('root'));

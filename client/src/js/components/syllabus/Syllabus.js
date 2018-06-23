4c26909addfe37a88d1968765312b43a8940aab2
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-06-23 11:56:17 +0800

    Remove CoursePage related components

--- client/src/js/components/syllabus/Syllabus.js
+++ client/src/js/components/syllabus/Syllabus.js
@@ -3,13 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import SyllabusSearch from './SyllabusSearch';
-import CoursePageContainer from '../../containers/CoursePageContainer';
 
 const Syllabus = ({ match }) => {
   return (
     <Switch>
       <Route exact path={`${match.url}`} component={SyllabusSearch} />
-      <Route path={`${match.url}/:courseId`} component={CoursePageContainer} />
       <Route component={SyllabusSearch} />
     </Switch>
   );

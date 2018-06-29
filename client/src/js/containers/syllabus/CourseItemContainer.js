362f1e31ab99508a4fd14df905c7dc3742536274
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-06-29 21:30:06 +0800

    CourseItemContainer.js: Limit number of courses added

--- client/src/js/containers/syllabus/CourseItemContainer.js
+++ client/src/js/containers/syllabus/CourseItemContainer.js
@@ -6,50 +6,52 @@ import { addCourse, removeCourse } from '../../actions/syllabus';
 import { getById } from '../../reducers/addedCourses';
 import CourseItem from '../../components/syllabus/CourseItem';
 
+const ADDED_COURSES_NUMBER_LIMIT = 50;
+
 class CourseItemContainer extends React.Component {
 
   handleAddCourse = event => {
     event.preventDefault();
     const { course, byId } = this.props;
-    const id = course._id;
     const occurrences = course.occurrences;
-    if (!(id in byId)) {
-      this.props.addCourse(id, course);
-      if (occurrences.some(o => o.day === -1 || o.start_period === -1)) {
-        Alert.warning('Course with undecided time cannot be displayed in timetable.', {
-          position: 'bottom',
-          effect: 'jelly',
-        });
-      } else {
-        Alert.success('Course added to timetable.', {
-          position: 'bottom',
-          effect: 'jelly',
-        });
-      }
+    if (Object.keys(byId).length >= ADDED_COURSES_NUMBER_LIMIT) {
+      Alert.error(`You cannot add more than ${ADDED_COURSES_NUMBER_LIMIT} courses`, {
+        position: 'bottom',
+        effect: 'jelly',
+      });
+      return;
     }
-  }
-
-  handleRemoveCourse = event => {
-    event.preventDefault();
-    const { course, byId } = this.props;
-    const id = course._id;
-    if (id in byId) {
-      this.props.removeCourse(course._id);
-      Alert.error('Course removed from timetable.', {
+    this.props.addCourse(course._id, course);
+    if (occurrences.some(o => o.day === -1 || o.start_period === -1)) {
+      Alert.warning('Course with undecided time cannot be displayed in timetable.', {
+        position: 'bottom',
+        effect: 'jelly',
+      });
+    } else {
+      Alert.success('Course added to timetable.', {
         position: 'bottom',
         effect: 'jelly',
       });
     }
   }
 
+  handleRemoveCourse = event => {
+    event.preventDefault();
+    const { course } = this.props;
+    this.props.removeCourse(course._id);
+    Alert.success('Course removed from timetable.', {
+      position: 'bottom',
+      effect: 'jelly',
+    });
+  }
+
   render() {
-    const { style, searchTerm, course, byId } = this.props;
+    const { searchTerm, course, byId } = this.props;
     const id = course._id;
-    const isAddable = !(id in byId)
+    const isAddable = !(id in byId);
     return (
       <CourseItem handleOnClick={isAddable ? this.handleAddCourse : this.handleRemoveCourse}
         isAddable = {isAddable}
-        style={style}
         searchTerm={searchTerm}
         course={course}
       />

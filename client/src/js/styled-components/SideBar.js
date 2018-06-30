8bfdd707d9905c3c91abced95767d967153232e3
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-07-01 00:51:07 +0800

    SideBar.js: Adjust style

--- client/src/js/styled-components/SideBar.js
+++ client/src/js/styled-components/SideBar.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const SideBar = styled('div')`
+  display: flex;
+  flex-direction: column;
   flex: ${props => `0 0 ${props.flexBasis};`}
   background-color: rgba(0,0,0,0.2);
 `

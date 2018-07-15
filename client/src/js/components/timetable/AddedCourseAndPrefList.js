bd60529b00d3735bd9c30b134840c4eea1ee49d3
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-07-15 17:22:03 +0800

    AddedCourseAndPrefList.js: Add <ul> since it is a list

--- client/src/js/components/timetable/AddedCourseAndPrefList.js
+++ client/src/js/components/timetable/AddedCourseAndPrefList.js
@@ -2,18 +2,25 @@ import React from 'react';
 
 import AddedCourseAndPrefItem from '../../containers/timetable/AddedCourseAndPrefItem';
 import { Wrapper } from '../../styled-components/Wrapper';
+import { UnstyledUList } from '../../styled-components/List';
 
+const ExtendedUnstyledUList = UnstyledUList.extend`
+  margin-top: 2em;
+  font-size: 14px;
+`
 
 const AddedCourseAndPrefList = ({addedCoursesAndPrefs}) => {
   return (
     <Wrapper>
-      {addedCoursesAndPrefs.map(addedCourseAndPref => (
-        <AddedCourseAndPrefItem
-          key={addedCourseAndPref.id}
-          addedCourseAndPref={addedCourseAndPref}
-        />
-      ))
-      }
+      <ExtendedUnstyledUList>
+        {addedCoursesAndPrefs.map(addedCourseAndPref => (
+          <AddedCourseAndPrefItem
+            key={addedCourseAndPref.id}
+            addedCourseAndPref={addedCourseAndPref}
+          />
+        ))
+        }
+      </ExtendedUnstyledUList>
     </Wrapper>
   )
 }

c86cd413c854738888c1ed134c5aac4bb2fbd52e
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-13 20:11:38 +0900

    Create reducer addedSemesterCourses.js

---
+++ client/src/js/reducers/addedSemesterCourses.js
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux';
+
+import addedById, * as fromById from './addedById';
+import addedCoursePrefs, * as fromPrefs from './addedCoursePrefs';
+
+const addedSemesterCourses = combineReducers({
+  prefs: addedCoursePrefs,
+  byId: addedById
+});
+
+export default addedSemesterCourses;
+
+export const getPrefs = state => state.prefs;
+
+export const getById = state => state.byId;
+
+export const getAddedCourses = state => {
+  if (!Object.keys(state.byId).length) return [];
+  return state.prefs.map(pref => (
+    fromById.getItem(state.byId, fromPrefs.getId(pref))
+  ));
+};
+
+export const getAddedCoursesAndPrefs = state => {
+  if (!Object.keys(state.byId).length) return [];
+  return state.prefs.map(pref => {
+    return {
+      ...pref,
+      course: fromById.getItem(state.byId, fromPrefs.getId(pref))
+    };
+  });
+};

cfee2c0556544310a906e9b7d97a8555dff98659
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-15 05:45:46 +0900

    addedCoursePrefs.js: Prettify

--- client/src/js/reducers/addedCoursePrefs.js
+++ client/src/js/reducers/addedCoursePrefs.js
@@ -1,5 +1,9 @@
-import { ADD_COURSE, REMOVE_COURSE,
-  CHANGE_COURSE_COLOR, TOGGLE_COURSE_VISIBILITY } from '../actions/types';
+import {
+  ADD_COURSE,
+  REMOVE_COURSE,
+  CHANGE_COURSE_COLOR,
+  TOGGLE_COURSE_VISIBILITY
+} from '../actions/types';
 
 const addedCoursePrefs = (state = [], action) => {
   switch (action.type) {
@@ -15,16 +19,18 @@ const addedCoursePrefs = (state = [], action) => {
     case REMOVE_COURSE:
       return state.filter(coursePref => coursePref.id !== action.payload.id);
     case CHANGE_COURSE_COLOR:
-      return state.map(coursePref =>
-        (coursePref.id === action.payload.id) ?
-        {...coursePref, color: action.payload.color} :
-        coursePref
+      return state.map(
+        coursePref =>
+          coursePref.id === action.payload.id
+            ? { ...coursePref, color: action.payload.color }
+            : coursePref
       );
     case TOGGLE_COURSE_VISIBILITY:
-      return state.map(coursePref =>
-        (coursePref.id === action.payload.id) ?
-        {...coursePref, visibility: !coursePref.visibility} :
-        coursePref
+      return state.map(
+        coursePref =>
+          coursePref.id === action.payload.id
+            ? { ...coursePref, visibility: !coursePref.visibility }
+            : coursePref
       );
     default:
       return state;

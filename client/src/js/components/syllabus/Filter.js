28d9fbf6009f40fb2a7dfadd89c5f633ed2b6b2e
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-15 00:42:29 +0900

    Filter.js: Add top margin to align with course results

--- client/src/js/components/syllabus/Filter.js
+++ client/src/js/components/syllabus/Filter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import stickybits from 'stickybits';
+import styled from 'styled-components';
 
 import FilterGroup from './FilterGroup';
 import { Wrapper } from '../../styled-components/Wrapper';
@@ -13,7 +14,15 @@ const FilterWrapper = Wrapper.extend`
 
 const FilterOverlay = Overlay.extend`
   padding: 0.5em 1em 1em 1em;
-`
+`;
+
+const FilterGroupWrapper = styled('div')`
+  background-color: #fff;
+  flex: 1 0 auto;
+  padding: 1em;
+  margin-top: 0.2em;
+  font-size: 14px;
+`;
 
 class Filter extends React.Component {
   constructor(props) {
@@ -22,20 +31,21 @@ class Filter extends React.Component {
 
     this.setWrapperRef = element => {
       this.wrapper = element;
-    }
+    };
 
     this.createStickyWrapper = () => {
       if (this.wrapper) {
-        this.stickyWrapper = stickybits(this.wrapper,
-          {stickyBitStickyOffset: 92});
+        this.stickyWrapper = stickybits(this.wrapper, {
+          stickyBitStickyOffset: 92
+        });
       }
-    }
+    };
 
     this.cleanupStickyWrapper = () => {
       if (this.stickyWrapper) {
         this.stickyWrapper.cleanup();
       }
-    }
+    };
   }
 
   componentDidMount() {
@@ -51,45 +61,38 @@ class Filter extends React.Component {
     const semesterLegend = 'Semesters';
     const semesterInputName = 'semester';
     const semesterInputs = [
-      {value: 'spring', label: 'Spring / Summer'},
-      {value: 'fall', label: 'Fall / Winter'},
-      {value: 'full', label: 'Full Year'}
+      { value: 'spring', label: 'Spring / Summer' },
+      { value: 'fall', label: 'Fall / Winter' },
+      { value: 'full', label: 'Full Year' }
     ];
     const schoolLegend = 'Schools';
     const schoolInputName = 'school';
     const schoolInputs = [
-      {value: 'sils', label: 'SILS'},
-      {value: 'pse', label: 'PSE'},
-      {value: 'fse', label: 'FSE'},
-      {value: 'cse', label: 'CSE'},
-      {value: 'ase', label: 'ASE'},
-      {value: 'cjl', label: 'CJL'}
+      { value: 'sils', label: 'SILS' },
+      { value: 'pse', label: 'PSE' },
+      { value: 'fse', label: 'FSE' },
+      { value: 'cse', label: 'CSE' },
+      { value: 'ase', label: 'ASE' },
+      { value: 'cjl', label: 'CJL' }
     ];
     const langLegend = 'Languages';
     const langInputName = 'lang';
     const langInputs = [
-      {value: 'en', label: 'English'},
-      {value: 'jp', label: 'Japanese'},
-      {value: 'others', label: 'Others'}
+      { value: 'en', label: 'English' },
+      { value: 'jp', label: 'Japanese' },
+      { value: 'others', label: 'Others' }
     ];
     const specialLegend = 'Special';
     const specialInputName = 'special';
     const specialInputs = [
-      {value: 'IPSE', label: 'IPSE'},
-      {value: 'ENUP', label: 'EN-based Undergrad Program'}
+      { value: 'IPSE', label: 'IPSE' },
+      { value: 'ENUP', label: 'EN-based Undergrad Program' }
     ];
     return (
       <FilterWrapper innerRef={this.setWrapperRef}>
         <FilterOverlay>
           <span>Filter by (under construction)</span>
-          <div
-            style={{
-              backgroundColor: "#fff",
-              flex: "1 0 auto",
-              padding: "1em",
-              fontSize: "14px"
-            }}
-          >
+          <FilterGroupWrapper>
             <FilterGroup
               legend={semesterLegend}
               inputName={semesterInputName}
@@ -110,10 +113,10 @@ class Filter extends React.Component {
               inputName={specialInputName}
               inputs={specialInputs}
             />
-          </div>
+          </FilterGroupWrapper>
         </FilterOverlay>
       </FilterWrapper>
-    )
+    );
   }
 }
 

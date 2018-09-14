827ca7dbc3261ceabf14a8bc1cf3ad55beb3edba
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-15 04:14:58 +0900

    localStorage.js: Update item name of state

--- client/src/localStorage.js
+++ client/src/localStorage.js
@@ -1,21 +1,23 @@
+const STATE_NAME = 'wasetime-state';
+
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem('state');
+    const serializedState = localStorage.getItem(STATE_NAME);
     if (serializedState === null) {
       return undefined;
     }
     return JSON.parse(serializedState);
   } catch (error) {
-      // In case of any errors, play safe and let reducers initialize the state.
-      return undefined;
+    // In case of any errors, play safe and let reducers initialize the state.
+    return undefined;
   }
-}
+};
 
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
+    localStorage.setItem(STATE_NAME, serializedState);
   } catch (error) {
     console.log(error);
   }
-}
+};

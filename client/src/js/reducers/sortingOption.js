7250f4bdd633c781f7bc5b8f961de25be54d4ae3
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-18 19:31:46 +0900

    reducers/sortingOptions.js: Rename action

--- client/src/js/reducers/sortingOption.js
+++ client/src/js/reducers/sortingOption.js
@@ -1,9 +1,9 @@
 import { ADDED_ORDER, COURSE_TITLE, COURSE_TIME } from '../data/sortingOptions';
-import { CHANGE_COURSE_SORTING_OPTION } from '../actions/types';
+import { CHANGE_COURSES_SORTING_OPTION } from '../actions/types';
 
 const sortingOption = (state = ADDED_ORDER, action) => {
   switch (action.type) {
-    case CHANGE_COURSE_SORTING_OPTION:
+    case CHANGE_COURSES_SORTING_OPTION:
       return action.payload.sortingOption;
     default:
       return state;

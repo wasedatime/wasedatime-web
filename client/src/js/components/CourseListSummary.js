9f0b27c7f85784b645a03628b8935edaa8216b91
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-19 01:26:45 +0900

    Extract CourseListSummary from AddedCourseAndPrefList

---
+++ client/src/js/components/CourseListSummary.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import SortingOptions from './SortingOptions';
+import { Wrapper, RowWrapper } from '../styled-components/Wrapper';
+import { InvisibleButton } from '../styled-components/Button';
+import { media } from '../utils/styledComponents';
+
+const CourseAddedMessageWrapper = RowWrapper.extend`
+  padding: 0.3em 1em;
+  ${media.tablet`padding: 0 1em;`};
+  ${media.phone`padding: 0 1em;`};
+  justify-content: space-between;
+`;
+
+const SortByButton = InvisibleButton.extend`
+  display: flex;
+  &:hover {
+    fill: #b51e36;
+    color: #b51e36;
+  }
+  &:focus {
+    fill: ${props => (props.isSortingOptionOpen ? '#b51e36;' : '#000;')};
+    color: ${props => (props.isSortingOptionOpen ? '#b51e36;' : '#000;')};
+  }
+  fill: ${props => (props.isSortingOptionOpen ? '#b51e36;' : '#000;')};
+  color: ${props => (props.isSortingOptionOpen ? '#b51e36;' : '#000;')};
+`;
+
+const CourseListSummary = ({
+  courses,
+  isSortingOptionOpen,
+  handleToggleSortingOptions,
+  selectedSortingOption,
+  handleChangeSortingOption
+}) => {
+  return (
+    <Wrapper>
+      <CourseAddedMessageWrapper>
+        <span>{`${courses.length} courses added `}</span>
+        <SortByButton
+          isSortingOptionOpen={isSortingOptionOpen}
+          onClick={handleToggleSortingOptions}
+        >
+          <span>Sort by&nbsp;</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+          >
+            <path d="M3 18h6v-2H3v2zM3 6v2h18V6H3zm0 7h12v-2H3v2z" />
+          </svg>
+        </SortByButton>
+      </CourseAddedMessageWrapper>
+      {isSortingOptionOpen && (
+        <SortingOptions
+          selectedSortingOption={selectedSortingOption}
+          handleChangeSortingOption={handleChangeSortingOption}
+        />
+      )}
+    </Wrapper>
+  );
+};
+
+export default CourseListSummary;

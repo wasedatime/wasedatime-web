f64de0583f3d53d2cf7ca1efd25c3ae3ea37ce50
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-19 02:09:18 +0900

    Create AddedCourseListSwitch for better composition

---
+++ client/src/js/containers/syllabus/AddedCourseListSwitch.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import SemesterTabs from '../../components/syllabus/SemesterTabs';
+import AddedCourseListContainer from './AddedCourseListContainer';
+import { Wrapper } from '../../styled-components/Wrapper';
+import { Overlay } from '../../styled-components/Overlay';
+
+class AddedCourseListSwitch extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedSemester: 'fall'
+    };
+  }
+
+  handleChangeSemester = targetSemester => {
+    if (this.state.selectedSemester !== targetSemester) {
+      this.setState({
+        selectedSemester: targetSemester
+      });
+    }
+  };
+
+  render() {
+    const { addedCourses } = this.props;
+    const { selectedSemester } = this.state;
+    const courses = addedCourses[selectedSemester];
+    return (
+      <Wrapper>
+        <Overlay>
+          <SemesterTabs
+            handleChangeSemester={this.handleChangeSemester}
+            semester={selectedSemester}
+          />
+          <AddedCourseListContainer
+            addedCourses={courses}
+            semesterKey={selectedSemester}
+          />
+        </Overlay>
+      </Wrapper>
+    );
+  }
+}
+
+export default AddedCourseListSwitch;

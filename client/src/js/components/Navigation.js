7e3df16c2ad39b839cc9a81551981daaece3258a
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-27 04:38:20 +0900

    Navigation.js: Use the colors imported from theme

--- client/src/js/components/Navigation.js
+++ client/src/js/components/Navigation.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faBook, faBuilding,
-  faBus } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendarAlt,
+  faBook,
+  faBuilding,
+  faBus
+} from '@fortawesome/free-solid-svg-icons';
 
 import { media } from '../styled-components/utils';
 
@@ -32,61 +36,62 @@ const StyledButton = styled('button')`
   outline: none;
   padding: 0;
   background-color: inherit;
-  color: #fff;
+  color: ${props =>
+    props.isClicked ? props.theme.colorPrimary : props.theme.white};
   &:hover {
-    color: #b51e36;
+    color: ${props => props.theme.colorPrimary};
   }
-`
+`;
 
 const StyledSpan = styled('span')`
   font-size: 0.55em;
   font-weight: 100;
   ${media.phone`font-size: 0.5em;`};
-`
+`;
 
-const Navigation = (props) => {
+const Navigation = props => {
   const pathname = props.location.pathname;
-  const navItems = [{
-    name: "Timetable",
-    path: "/timetable",
-    icon: faCalendarAlt
-  },{
-    name: "Syllabus",
-    path: "/syllabus",
-    icon: faBook
-  },{
-    name: "RoomFinder",
-    path: "/roomfinder",
-    icon: faBuilding
-  },{
-    name: "Bus",
-    path: "/bus",
-    icon: faBus
-  }];
-  const styledLinks = navItems.map((item) => {
-    const itemName = item["name"];
-    const itemPath = item["path"];
-    const itemIcon = item["icon"];
-    const buttonStyle = pathname.includes(itemPath)
-      ? {color: "#b51e36"} : {};
-    const fontBase = <FontAwesomeIcon icon={itemIcon}
-      size="2x" transform="shrink-2" />
+  const navItems = [
+    {
+      name: 'Timetable',
+      path: '/timetable',
+      icon: faCalendarAlt
+    },
+    {
+      name: 'Syllabus',
+      path: '/syllabus',
+      icon: faBook
+    },
+    {
+      name: 'RoomFinder',
+      path: '/roomfinder',
+      icon: faBuilding
+    },
+    {
+      name: 'Bus',
+      path: '/bus',
+      icon: faBus
+    }
+  ];
+  const styledLinks = navItems.map(item => {
+    const itemName = item['name'];
+    const itemPath = item['path'];
+    const itemIcon = item['icon'];
+    const isClicked = pathname.includes(itemPath);
+    const fontBase = (
+      <FontAwesomeIcon icon={itemIcon} size="2x" transform="shrink-2" />
+    );
     return (
-        <StyledLink to={itemPath} key={itemPath}>
-          <StyledButton style={buttonStyle}>
-            {fontBase}
-            <StyledSpan>{itemName}</StyledSpan>
-          </StyledButton>
-        </StyledLink>
+      <StyledLink to={itemPath} key={itemPath}>
+        <StyledButton isClicked={isClicked}>
+          {fontBase}
+          <StyledSpan>{itemName}</StyledSpan>
+        </StyledButton>
+      </StyledLink>
     );
   });
 
-
-  return (
-    <StyledNav>
-      {styledLinks}
-    </StyledNav>
-  );
+  return <StyledNav>{styledLinks}</StyledNav>;
 };
 
 export default withRouter(Navigation);

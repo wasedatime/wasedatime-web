e9c86bc909500b0b9149840a787c52883bf6e3d2
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-26 21:25:05 +0900

    App.js: Pass down normalTheme in to ThemeProvider

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Alert from 'react-s-alert';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
+import { normalTheme } from '../styled-components/theme';
 import 'react-s-alert/dist/s-alert-default.css';
 import '../../styles/s-alert-custom.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
@@ -33,22 +34,24 @@ const StyledMain = styled('main')`
 
 const App = () => {
   return (
-    <Wrapper>
-      <Header />
-      <StyledMain>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/timetable" />} />
-          <Route path="/about" component={About} />
-          <Route path="/timetable" component={TimetableContainer} />
-          <Route path="/syllabus" component={Syllabus} />
-          <Route path="/roomfinder" component={RoomFinder} />
-          <Route path="/bus" component={Bus} />
-          <Route component={NotFound} />
-        </Switch>
-      </StyledMain>
-      <FooterContainer />
-      <Alert stack={{ limit: 2 }} timeout={3000} />
-    </Wrapper>
+    <ThemeProvider theme={normalTheme}>
+      <Wrapper>
+        <Header />
+        <StyledMain>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/timetable" />} />
+            <Route path="/about" component={About} />
+            <Route path="/timetable" component={TimetableContainer} />
+            <Route path="/syllabus" component={Syllabus} />
+            <Route path="/roomfinder" component={RoomFinder} />
+            <Route path="/bus" component={Bus} />
+            <Route component={NotFound} />
+          </Switch>
+        </StyledMain>
+        <FooterContainer />
+        <Alert stack={{ limit: 1 }} timeout={3000} />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 

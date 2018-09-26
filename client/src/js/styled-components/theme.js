7b9cceb9ceb0fe90513bc817d643c8160f535c46
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-27 04:01:10 +0900

    theme.js: Create more complex reusable themes

--- client/src/js/styled-components/theme.js
+++ client/src/js/styled-components/theme.js
@@ -1,41 +1,57 @@
 // Adapted from https://medium.com/codyhouse/create-your-design-system-part-3-colors-798e4729921f
 
-export const normalTheme = {
-  //main colors
-  colorPrimary: '#b51e36',
-
-  //shades
+const shades = {
   black: '#000',
   grey2: '#222',
   grey4: '#444',
   grey7: '#777',
   grey12: '#ccc',
   grey14: '#eee',
-  white: '#fff',
+  white: '#fff'
+};
+
+export const normalTheme = {
+  //main colors
+  colorPrimary: '#b51e36',
 
   //declarative colors
   red: '#ce0115',
   green: '#48af37',
   blue: '#6495ed',
 
+  //semantic colors
+  timetableBgColor: shades.white
+};
+
+export const darkTheme = {
+  //main colors
+  colorPrimary: '#b51e36',
+
+  //declarative colors
+  red: '#ce0115',
+  green: '#48af37',
+  blue: '#6495ed'
+};
+
+export const timetableTheme = {
   /*
   Incredibly useful site:
   https://www.materialpalette.com/colors
   */
-  timetableColor0: '#580505',
-  timetableBgColor0: '#ef9a9a',
-  timetableColor1: '#862505',
-  timetableBgColor1: '#ffcc80',
-  timetableColor2: '#845125',
-  timetableBgColor2: '#fff59d',
-  timetableColor3: '#114615',
-  timetableBgColor3: '#a5d6a7',
-  timetableColor4: '#0c2a58',
-  timetableBgColor4: '#90caf9',
-  timetableColor5: '#221361',
-  timetableBgColor5: '#b39ddb',
-  timetableColor6: '#3e2723',
-  timetableBgColor6: '#c7a59a',
-  timetableColor7: '#222',
-  timetableBgColor7: '#aaa'
+  color0: '#580505',
+  bgColor0: '#ef9a9a',
+  color1: '#862505',
+  bgColor1: '#ffcc80',
+  color2: '#845125',
+  bgColor2: '#fff59d',
+  color3: '#114615',
+  bgColor3: '#a5d6a7',
+  color4: '#0c2a58',
+  bgColor4: '#90caf9',
+  color5: '#221361',
+  bgColor5: '#b39ddb',
+  color6: '#3e2723',
+  bgColor6: '#c7a59a',
+  color7: '#222',
+  bgColor7: '#aaa'
 };

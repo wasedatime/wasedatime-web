2d1e444afb4d51c5d1e5fc0b7c182b6834ffe4e6
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-09-27 03:43:03 +0900

    theme.js: Add colors

--- client/src/js/styled-components/theme.js
+++ client/src/js/styled-components/theme.js
@@ -1,3 +1,41 @@
+// Adapted from https://medium.com/codyhouse/create-your-design-system-part-3-colors-798e4729921f
+
 export const normalTheme = {
-  primaryColor: '#b51e36'
+  //main colors
+  colorPrimary: '#b51e36',
+
+  //shades
+  black: '#000',
+  grey2: '#222',
+  grey4: '#444',
+  grey7: '#777',
+  grey12: '#ccc',
+  grey14: '#eee',
+  white: '#fff',
+
+  //declarative colors
+  red: '#ce0115',
+  green: '#48af37',
+  blue: '#6495ed',
+
+  /*
+  Incredibly useful site:
+  https://www.materialpalette.com/colors
+  */
+  timetableColor0: '#580505',
+  timetableBgColor0: '#ef9a9a',
+  timetableColor1: '#862505',
+  timetableBgColor1: '#ffcc80',
+  timetableColor2: '#845125',
+  timetableBgColor2: '#fff59d',
+  timetableColor3: '#114615',
+  timetableBgColor3: '#a5d6a7',
+  timetableColor4: '#0c2a58',
+  timetableBgColor4: '#90caf9',
+  timetableColor5: '#221361',
+  timetableBgColor5: '#b39ddb',
+  timetableColor6: '#3e2723',
+  timetableBgColor6: '#c7a59a',
+  timetableColor7: '#222',
+  timetableBgColor7: '#aaa'
 };

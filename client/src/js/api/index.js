804b550b6254d42f8bd1dba2225a60b088f07ace
OscarWang114 <chihhaowang.oscar@gmail.com>
2018-11-28 15:19:34 +0900

    index.js: Add courseListPSE static url

--- client/src/js/api/index.js
+++ client/src/js/api/index.js
@@ -1,10 +1,11 @@
 const API_STATIC_BASE_URL =
-  process.env.NODE_ENV === 'production' ?
-  '/api/static/' :
-  'https://wasetime.com/api/static/';
-const YEAR = '2018-2019/'
+  process.env.NODE_ENV === 'production'
+    ? '/api/static/'
+    : 'https://wasetime.com/api/static/';
+const YEAR = '2018-2019/';
 const API_STATIC_URL = API_STATIC_BASE_URL + YEAR;
 export const wasetimeApiStatic = {
-  scraperStats: API_STATIC_URL + "scraper_stats/index.json",
-  courseListAll: API_STATIC_URL + "course_list_all.json"
-}
+  scraperStats: API_STATIC_URL + 'scraper_stats/index.json',
+  courseListAll: API_STATIC_URL + 'course_list_all.json',
+  courseListPSE: API_STATIC_URL + 'course_list_PSE.json'
+};

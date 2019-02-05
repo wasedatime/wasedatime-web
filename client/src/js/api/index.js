1af01e791895ea17a5c0cad84d2bb4c1182c92d4
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-02-05 18:28:46 +0900

    api/index.js: Refactor conditional statement

--- client/src/js/api/index.js
+++ client/src/js/api/index.js
@@ -1,14 +1,16 @@
 let API_STATIC_BASE_URL;
-if (process.env.NODE_ENV === 'production') {
-  API_STATIC_BASE_URL = '/api/static/';
-} else if (process.env.NODE_ENV === 'development') {
-  if (process.env.REACT_APP_ENV === 'development-remote') {
+switch (process.env.NODE_ENV) {
+  case 'production':
     API_STATIC_BASE_URL = '/api/static/';
-  } else {
-    API_STATIC_BASE_URL = 'https://wasetime.com/api/static/';
-  }
-} else {
-  API_STATIC_BASE_URL = 'https://wasetime.com/api/static/';
+    break;
+  case 'development':
+    API_STATIC_BASE_URL =
+      process.env.REACT_APP_ENV === 'development-remote'
+        ? '/api/static/'
+        : 'https://dev.wasetime.com/api/static/';
+    break;
+  default:
+    API_STATIC_BASE_URL = 'https://dev.wasetime.com/api/static/';
 }
 
 const YEAR = '2018-2019/';

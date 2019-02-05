862df54322320cf319fedb4e61910dc229bcd2bc
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-02-05 18:27:40 +0900

    Remote setupProxy.js since we fetch from dev server instead of  proxy /api

--- client/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-// A required file for create-react-app proxy configuration after 2.0.3
-
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
-};

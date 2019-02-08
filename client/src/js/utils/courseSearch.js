a82788b89151cbf45eac3ff88cb2772ce74af55e
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-02-09 02:40:33 +0900

    courseSearch.js: Support searching and sorting courses in en and jp

--- client/src/js/utils/courseSearch.js
+++ client/src/js/utils/courseSearch.js
@@ -1,34 +1,52 @@
 import sortBy from 'lodash/sortBy';
 
+// Unicode for Japanese: http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+const jpRegex = '\u3040-\u309f\u30a0-\u30ff\u4e00-\u9faf';
+
 export const tokenize = string => {
-  return string.trim().split(/\W+/g);
+  const regex = new RegExp(`[^A-Za-z0-9${jpRegex}]+`);
+  return string.trim().split(regex);
 };
 
-export const regexify = string => {
-  const terms = string.trim().replace(/\W+/g, '\\W+');
-  const reg = RegExp(`\\b${terms}`, 'i');
-  return reg;
+export const regexify = token => {
+  // Unnecessary trimming and replacing of \W characters
+  // const terms = string.trim().replace(/\W+/g, '\\W+');
+  const regex = new RegExp(`\\b${token}`, 'i');
+  return regex;
 };
 
-export const searchCourses = (searchTerm, courses) => {
+export const getCourseTitleAndInstructor = (course, searchLang) => {
+  return searchLang === 'en'
+    ? { title: course.title, instructor: course.instructor }
+    : { title: course.title_jp, instructor: course.instructor_jp };
+};
+
+export const searchCourses = (searchTerm, courses, searchLang) => {
   const searchRegexes = tokenize(searchTerm).map(regexify);
-
   return courses.filter(course => {
+    const { title, instructor } = getCourseTitleAndInstructor(
+      course,
+      searchLang
+    );
     return searchRegexes.every(regex => {
-      return regex.test(course.title) || regex.test(course.instructor);
+      return regex.test(title) || regex.test(instructor);
     });
   });
 };
 
-export const sortCourses = (searchTerm, courses) => {
+export const sortCourses = (searchTerm, courses, searchLang) => {
   const searchRegexes = tokenize(searchTerm).map(regexify);
 
   return sortBy(courses, course => {
+    const { title, instructor } = getCourseTitleAndInstructor(
+      course,
+      searchLang
+    );
     let sum = 0;
     for (let i = 0; i < searchRegexes.length; i++) {
-      if (searchRegexes[i].test(course.title)) {
+      if (searchRegexes[i].test(title)) {
         sum += 1;
-      } else if (searchRegexes[i].test(course.instructor)) {
+      } else if (searchRegexes[i].test(instructor)) {
         sum += 2;
       } else {
         sum += 3;

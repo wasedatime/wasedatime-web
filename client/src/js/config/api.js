e82191aa058ac83196bc63a7c0aaff4f0303abab
NeroArcher <59641121+NeroArcher@users.noreply.github.com>
2020-06-02 01:39:14 +0900

    Updated 2020 syllabus

--- client/src/js/config/api.js
+++ client/src/js/config/api.js
@@ -2,7 +2,7 @@ const API_STATIC_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "/api/static/"
     : "https://staging.wasedatime.com/api/static/";
-const YEAR = "2019-2020/";
+const YEAR = "2020-2021/";
 const API_STATIC_URL = API_STATIC_BASE_URL + YEAR;
 export const wasetimeApiStatic = {
   scraperStats: API_STATIC_URL + "scraper_stats/index.json",

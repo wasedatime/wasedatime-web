37c4e4349a48b883faff63e9e3905805d9b72288
YHHaoAreYou <mlapenang1998@gmail.com>
2020-07-26 00:36:43 +0900

    Edit: make related courses button more clickable

--- client/src/js/components/courseEvals/RelatedCoursesButton.js
+++ client/src/js/components/courseEvals/RelatedCoursesButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faHandPointUp } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { withNamespaces } from "react-i18next";
@@ -10,14 +10,14 @@ import { InvisibleButton } from "../../styled-components/Button";
 const ExtendedInvisibleButton = styled(InvisibleButton)`
   position: fixed;
   z-index: 3002;
-  width: ${props => props.isModalOpen ? '56px' : '100px'};
+  width: ${props => props.isModalOpen ? '56px' : '130px'};
   height: 56px;
   justify-content: center;
   display: flex;
   align-items: center;
   background-color: #b51e36;
   color: white;
-  border-radius: ${props => props.isModalOpen ? '30px' : '10px'};
+  border-radius: ${props => props.isModalOpen ? '30px' : '20px'};
   bottom: 20px;
   right: 20px;
   box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.4), 0 6px 10px 0 rgba(0, 0, 0, 0.2),
@@ -25,7 +25,21 @@ const ExtendedInvisibleButton = styled(InvisibleButton)`
   -webkit-tap-highlight-color: transparent;
 `;
 
-const FilterButton = ({ isModalOpen, handleToggleModal, lng }) => {
+const RelatedCoursesButtonFlex = styled('div')`
+  display: flex;
+  flex-direction: row;
+`;
+
+const HandPointUpIconWrapper = styled('div')`
+  flex: 1;
+  text-align: right;
+`;
+
+const RelatedCoursesButtonText = styled('div')`
+  flex: 3;
+`;
+
+const FilterButton = ({ isModalOpen, handleToggleModal, t }) => {
   return (
     <ExtendedInvisibleButton onClick={handleToggleModal} isModalOpen={isModalOpen}>
       {
@@ -33,7 +47,20 @@ const FilterButton = ({ isModalOpen, handleToggleModal, lng }) => {
           icon={faTimes}
           size="2x"
           transform="shrink-3"
-        /> : "Related Courses"
+        /> : (
+          <RelatedCoursesButtonFlex>
+            <HandPointUpIconWrapper>
+              <FontAwesomeIcon
+                icon={faHandPointUp}
+                size="2x"
+                transform="shrink-3"
+              />
+            </HandPointUpIconWrapper>
+            <RelatedCoursesButtonText>
+              {t(`courseEvals.Related courses`)}
+            </RelatedCoursesButtonText>
+          </RelatedCoursesButtonFlex>
+        )
       }
     </ExtendedInvisibleButton>
   );

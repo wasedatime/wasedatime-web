219c850a480d9acaf4f5de904936a0c79a6aa6b4
OscarWang114 <chihhaowang.oscar@gmail.com>
2020-08-08 21:52:18 +0800

    Create a reusable FloatingActionButton for FilterButton and RelatedCoursesButton

--- client/src/js/components/courseEvals/RelatedCoursesButton.js
+++ client/src/js/components/courseEvals/RelatedCoursesButton.js
@@ -1,74 +1,65 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes, faHandPointUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTimes,
+  faSearchPlus,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { withNamespaces } from "react-i18next";
 
-import { InvisibleButton } from "../../styled-components/Button";
+import { FloatingActionButton } from "../../styled-components/Button";
 
-const ExtendedInvisibleButton = styled(InvisibleButton)`
-  position: fixed;
-  z-index: 3002;
-  width: ${props => props.isModalOpen ? '56px' : '130px'};
-  height: 56px;
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  background-color: #b51e36;
-  color: white;
-  border-radius: ${props => props.isModalOpen ? '30px' : '20px'};
-  bottom: 20px;
-  right: 20px;
-  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.4), 0 6px 10px 0 rgba(0, 0, 0, 0.2),
-    0 1px 18px 0 rgba(0, 0, 0, 0.15);
-  -webkit-tap-highlight-color: transparent;
-`;
-
-const RelatedCoursesButtonFlex = styled('div')`
+const IconTextWrapper = styled("div")`
+  flex: 1;
   display: flex;
   flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0 10px;
 `;
 
-const HandPointUpIconWrapper = styled('div')`
+const IconWrapper = styled("div")`
   flex: 1;
-  text-align: right;
 `;
 
-const RelatedCoursesButtonText = styled('div')`
-  flex: 3;
+const ButtonText = styled("div")`
+  flex: 2;
 `;
 
-const FilterButton = ({ isModalOpen, handleToggleModal, t }) => {
+const RelatedCoursesButton = ({ isModalOpen, handleToggleModal, t }) => {
+  const width = isModalOpen ? "56px" : "130px";
+  const borderRadius = isModalOpen ? "30px" : "20px";
+
   return (
-    <ExtendedInvisibleButton onClick={handleToggleModal} isModalOpen={isModalOpen}>
-      {
-        isModalOpen ? <FontAwesomeIcon
-          icon={faTimes}
-          size="2x"
-          transform="shrink-3"
-        /> : (
-          <RelatedCoursesButtonFlex>
-            <HandPointUpIconWrapper>
-              <FontAwesomeIcon
-                icon={faHandPointUp}
-                size="2x"
-                transform="shrink-3"
-              />
-            </HandPointUpIconWrapper>
-            <RelatedCoursesButtonText>
-              {t(`courseEvals.Related courses`)}
-            </RelatedCoursesButtonText>
-          </RelatedCoursesButtonFlex>
-        )
-      }
-    </ExtendedInvisibleButton>
+    <FloatingActionButton
+      onClick={handleToggleModal}
+      width={width}
+      borderRadius={borderRadius}
+    >
+      {isModalOpen ? (
+        <FontAwesomeIcon icon={faTimes} size="2x" transform="shrink-3" />
+      ) : (
+        <IconTextWrapper>
+          <IconWrapper>
+            <FontAwesomeIcon
+              icon={faSearchPlus}
+              size="2x"
+              transform="shrink-3"
+            />{" "}
+          </IconWrapper>
+          <ButtonText>{t(`courseEvals.Related courses`)}</ButtonText>
+        </IconTextWrapper>
+      )}
+    </FloatingActionButton>
   );
 };
 
-export default withNamespaces("translation")(FilterButton);
+export default withNamespaces("translation")(RelatedCoursesButton);
 
-FilterButton.propTypes = {
+RelatedCoursesButton.propTypes = {
   isModalOpen: PropTypes.bool.isRequired,
-  handleToggleModal: PropTypes.func.isRequired
+  handleToggleModal: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
 };

5ae83b7addfd0f455826d103be5cf28bf64788c6
Yong Hao Hu <mlapenang1998@gmail.com>
2020-09-05 23:35:28 +0800

    Edit course_evals API url

--- client/src/js/config/api.js
+++ client/src/js/config/api.js
@@ -6,7 +6,7 @@ const YEAR = "2020-2021/";
 const API_STATIC_URL = API_STATIC_BASE_URL + YEAR;
 const API_COURSE_EVALS_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "/api/course_evals/"
+    ? "/api/course_evals"
     : "https://staging.wasedatime.com/api/course_evals_test";
 export const wasetimeApiStatic = {
   scraperStats: API_STATIC_URL + "scraper_stats/index.json",

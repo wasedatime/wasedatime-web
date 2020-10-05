28d253a1eb80f64e80ce544a038a7da4320fe6ab
Yong Hao Hu <mlapenang1998@gmail.com>
2020-10-05 18:53:34 +0800

    change api url

--- client/src/js/config/api.js
+++ client/src/js/config/api.js
@@ -1,7 +1,9 @@
-const API_STATIC_BASE_URL = "https://wasedatime-syllabus.s3-ap-northeast-1.amazonaws.com/";
+const API_STATIC_BASE_URL =
+  "https://wasedatime-syllabus.s3-ap-northeast-1.amazonaws.com/";
 const YEAR = "2020-2021/";
 const API_STATIC_URL = API_STATIC_BASE_URL + YEAR;
-const API_COURSE_EVALS_BASE_URL = "https://biq3vr83u0.execute-api.ap-northeast-1.amazonaws.com/staging/course_reviews";
+const API_COURSE_EVALS_BASE_URL =
+  "https://api.wasedatime.com/staging/course_reviews";
 export const wasetimeApiStatic = {
   scraperStats: API_STATIC_URL + "scraper_stats/index.json",
   courseListAll: API_STATIC_URL + "course_list_all_min.json",

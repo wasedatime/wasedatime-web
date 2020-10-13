2c586ce5d95b15db5e6ba028a27d5ad76f127691
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-10-13 11:13:39 +0900

    modify travis script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ set -x
 
 git checkout "$TRAVIS_BRANCH"
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then echo "trigger master webhook"; fi
+if [ "$TRAVIS_BRANCH" == 'master' ] ; then
+  curl -X POST -d {} "https://webhooks.amplify.$AWS_ZONE.amazonaws.com/prod/webhooks?id=$AWS_WEBHOOK_ID_MAIN&token=$AWS_WEBHOOK_TOKEN_MAIN&operation=startbuild" -H "Content-Type:application/json"
+fi
 
-if [ $TRAVIS_BRANCH == 'release/'* ] ; then echo "trigger release webhook"; fi
-
-if [ $TRAVIS_BRANCH == 'develop' ] ; then
-    curl -X POST -d {} "https://webhooks.amplify.$AWS_ZONE.amazonaws.com/prod/webhooks?id=$AWS_WEBHOOK_ID&token=$AWS_WEBHOOK_TOKEN&operation=startbuild" -H "Content-Type:application/json"
+if [ "$TRAVIS_BRANCH" == 'develop' ] ; then
+    curl -X POST -d {} "https://webhooks.amplify.$AWS_ZONE.amazonaws.com/prod/webhooks?id=$AWS_WEBHOOK_ID_DEV&token=$AWS_WEBHOOK_TOKEN_DEV&operation=startbuild" -H "Content-Type:application/json"
 fi

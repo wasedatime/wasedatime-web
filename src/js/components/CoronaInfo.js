9bb921aff346b11682406d823d100354e22ac3d3
YHHaoAreYou <mlapenang1998@gmail.com>
2020-10-19 20:18:10 +0900

    read data from John Hopkins Uni

--- src/js/components/CoronaInfo.js
+++ src/js/components/CoronaInfo.js
@@ -5,6 +5,7 @@ import { Wrapper } from "../styled-components/Wrapper";
 import { Overlay } from "../styled-components/Overlay";
 import styled from "styled-components";
 import { media } from "../styled-components/utils";
+import axios from "axios";
 
 const ExtendedOverlay = styled(Overlay)`
   align-items: center;
@@ -51,28 +52,102 @@ const options = {
   }
 }
 
-const CoronaInfo = () => {
-  return (
-    <Wrapper>
-      <Helmet>
-        <title>WasedaTime - Corona Info</title>
-        <meta
-          name="description"
-          content="Information about Coronavirus."
-        />
-        <meta property="og:title" content="WasedaTime - Corona Info" />
-        <meta
-          property="og:description"
-          content="Information about Coronavirus."
-        />
-        <meta property="og:site_name" content="WasedaTime - Corona Info" />
-      </Helmet>
+class CoronaInfo extends React.Component {
+  async componentDidMount () {
+    const csvData = await this.getInfo();
+    const rows = csvData.split(/\r\n|\n/);
+    const headers = rows[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+    const usedHeaders = ["Active", "Confirmed", "Country_Region", "Deaths", "Last_Update", "Province_State", "Recovered"];
 
-      <ExtendedOverlay>
-        <Line data={data} options={options} />
-      </ExtendedOverlay>
-    </Wrapper>
-  );
-};
+    const list = [];
+    for (let i = 1; i < rows.length; i++) {
+      const columns = rows[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      if (headers && columns.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          if (headers[j] && usedHeaders.includes(headers[j])) {
+            let d = columns[j];
+            if (d.length > 0) {
+              if (d[0] === '"') d = d.substring(1, d.length - 1);
+              if (d[d.length - 1] === '"') d = d.substring(d.length - 2, 1);
+            }
+            obj[headers[j]] = d;
+          }
+        }
+
+        // remove the blank rows
+        if (Object.values(obj).filter(x => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    }
+
+    var listByCountry = {};
+    var listTokyo = {};
+
+    for (let i = 0; i < list.length; i++) {
+      if (listByCountry[list[i]["Country_Region"]]) {
+        listByCountry[list[i]["Country_Region"]]["Confirmed"] += parseInt(list[i]["Confirmed"]);
+        listByCountry[list[i]["Country_Region"]]["Active"] += parseInt(list[i]["Active"]);
+        listByCountry[list[i]["Country_Region"]]["Recovered"] += parseInt(list[i]["Recovered"]);
+        listByCountry[list[i]["Country_Region"]]["Deaths"] += parseInt(list[i]["Deaths"]);
+      } else {
+        listByCountry[list[i]["Country_Region"]] = {
+          "Confirmed": 0,
+          "Active": 0,
+          "Recovered": 0,
+          "Deaths": 0,
+        };
+      }
+      if (list[i]["Province_State"] === "Tokyo") {
+        listTokyo = {
+          "Confirmed": parseInt(list[i]["Confirmed"]),
+          "Active": parseInt(list[i]["Active"]),
+          "Recovered": parseInt(list[i]["Recovered"]),
+          "Deaths": parseInt(list[i]["Deaths"]),
+        };
+      }
+    }
+
+    console.log(list);
+    console.log(listByCountry);
+    console.log(listTokyo);
+  }
+
+  async getInfo () {
+    try {
+      const res = await axios.get(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/10-17-2020.csv"
+      );
+      return res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render () {
+    return (
+      <Wrapper>
+        <Helmet>
+          <title>WasedaTime - Corona Info</title>
+          <meta
+            name="description"
+            content="Information about Coronavirus."
+            />
+          <meta property="og:title" content="WasedaTime - Corona Info" />
+          <meta
+            property="og:description"
+            content="Information about Coronavirus."
+            />
+          <meta property="og:site_name" content="WasedaTime - Corona Info" />
+        </Helmet>
+
+        <ExtendedOverlay>
+          <Line data={data} options={options} />
+        </ExtendedOverlay>
+      </Wrapper>
+    );
+  }
+}
 
 export default CoronaInfo;

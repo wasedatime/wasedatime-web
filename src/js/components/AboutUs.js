734282482fe3059080fac3a10bbaa12a907e1df9
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-30 00:40:54 +0900

    Attempt to improve font-size of tabs

--- src/js/components/AboutUs.js
+++ src/js/components/AboutUs.js
@@ -1,22 +1,29 @@
 import React from "react";
-import { Tab } from 'semantic-ui-react'
+import styled from "styled-components";
+import { Tab, Menu } from "semantic-ui-react";
 
+import OurMission from "./OurMission.js";
+import MeetOurTeam from "./MeetOurTeam.js";
+import JoinUs from "./JoinUs.js";
 
-import OurMission from './OurMission.js'
-import MeetOurTeam from './MeetOurTeam.js'
-import JoinUs from './JoinUs.js'
-
-
-
+// 想要override semantic的style需要用「!important」
+// 內容的字變大了，但標籤的字沒受到影響？！
+const LargeTab = styled(Tab)`
+  font-size: 2em !important;
+`;
 
 const panes = [
-    { menuItem: 'Our Mission', render: () => <OurMission />, size:"massive"},
-    { menuItem: 'Meet Our Team', render: () => <MeetOurTeam /> },
-    { menuItem: 'Join Us', render: () => <JoinUs /> },
-  
-]
+  {
+    // 要直接override menuItem本身的style（個案）
+    menuItem: <Menu.Item style={{ fontSize: "2em" }}>Our Mission</Menu.Item>,
+    render: () => <OurMission />,
+  },
+  // 其他的就麻煩你修改然後繼續開發～
+  { menuItem: "Meet Our Team", render: () => <MeetOurTeam /> },
+  { menuItem: "Join Us", render: () => <JoinUs /> },
+];
 const TabExampleSecondary = () => (
-    <Tab menu={{ secondary: true }} panes={panes} />
-)
+  <LargeTab menu={{ secondary: true }} panes={panes} />
+);
 
-export default TabExampleSecondary
+export default TabExampleSecondary;

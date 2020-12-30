85a8006beac349fd72669906bf74d1fe9c88cc83
顧嘉賢 <gujiaxian1997@gmail.com>
2020-12-30 15:57:05 +0900

    adjust tabs' size

--- src/js/components/AboutUs.js
+++ src/js/components/AboutUs.js
@@ -9,22 +9,29 @@ import JoinUs from "./JoinUs.js";
 // 想要override semantic的style需要用「!important」
 // 內容的字變大了，但標籤的字沒受到影響？！
 const LargeTab = styled(Tab)`
-  font-size: 2em !important;
   margin: 0 auto;
 `;
 
 const panes = [
   {
     // 要直接override menuItem本身的style（個案）
-    menuItem: <Menu.Item style={{ fontSize: "2em" }}>Our Mission</Menu.Item>,
+    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Our Mission</Menu.Item>,
     render: () => <OurMission />,
   },
   // 其他的就麻煩你修改然後繼續開發～
-  { menuItem: "Meet Our Team", render: () => <MeetOurTeam /> },
-  { menuItem: "Join Us", render: () => <JoinUs /> },
+  { 
+    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Meet Our Team</Menu.Item>, 
+    render: () => <MeetOurTeam /> 
+  },
+  { 
+    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Join Us</Menu.Item>, 
+    render: () => <JoinUs /> 
+  },
 ];
 const TabExampleSecondary = () => (
   <LargeTab menu={{ secondary: true }} panes={panes} />
 );
 
 export default TabExampleSecondary;
+
+

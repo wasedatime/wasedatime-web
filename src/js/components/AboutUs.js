dc4e9aa07aa41518b4a8d2a5b488acddae82c018
顧嘉賢 <gujiaxian1997@gmail.com>
2021-01-02 20:57:57 +0900

    our mission content

--- src/js/components/AboutUs.js
+++ src/js/components/AboutUs.js
@@ -9,22 +9,23 @@ import JoinUs from "./JoinUs.js";
 // 想要override semantic的style需要用「!important」
 // 內容的字變大了，但標籤的字沒受到影響？！
 const LargeTab = styled(Tab)`
-  margin: 0 auto;
+    // margin: 100px 100px !important; 
+    // border: solid;
 `;
-
+{/* <img></img> */}
 const panes = [
   {
     // 要直接override menuItem本身的style（個案）
-    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Our Mission</Menu.Item>,
+    menuItem: <Menu.Item style={{ fontSize:"1.5em"}}>Our Mission</Menu.Item>,
     render: () => <OurMission />,
   },
   // 其他的就麻煩你修改然後繼續開發～
   { 
-    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Meet Our Team</Menu.Item>, 
+    menuItem: <Menu.Item style={{ fontSize:"1.5em"}}>Meet Our Team</Menu.Item>, 
     render: () => <MeetOurTeam /> 
   },
   { 
-    menuItem: <Menu.Item style={{ fontSize:"2em"}}>Join Us</Menu.Item>, 
+    menuItem: <Menu.Item style={{ fontSize:"1.5em"}}>Join Us</Menu.Item>, 
     render: () => <JoinUs /> 
   },
 ];
--- src/js/components/OurMission.js
+++ src/js/components/OurMission.js
@@ -1,8 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled("h2")`
+    width: 50%;
+    font-weight:bold;
+`;
+const MainParagraph = styled("p")`
+    width: 100%;
+    background-color:red;
+`;
 
 const OurMission = () => {
     return (
-        <p>aaa</p>
+        
+        <MainParagraph>as ajsiodjoisjfg sdfgjisdjgiodfssgosidjfogifj</MainParagraph>,
+        <Title>we are On A Mission</Title>
+        // <p>WasedaTime has been working on supporting students' academic
+        // activities since launching. Its well-tested Syllabus Search feature is
+        // favored by thousands of students. More than 50% of international
+        // students are using this website. We never expect this could happen and
+        // are always grateful for all our users.</p>
     );
 }
 

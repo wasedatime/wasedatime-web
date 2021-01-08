41a64b76bc538ced0a407b4a89978cd62adf8fe2
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-08 21:47:51 +0900

    reducer for addedCourses idsBySchool

--- src/js/reducers/addedIdsBySchool.js
+++ src/js/reducers/addedIdsBySchool.js
@@ -3,9 +3,34 @@ import { ADD_COURSE, REMOVE_COURSE, SAVE_TIMETABLE } from "../actions/types";
 const addedIdsBySchool = (state = {}, action) => {
   switch (action.type) {
     case ADD_COURSE:
-      return state;
+      const school = action.payload.course.r;
+      const newIds = state[school]
+        ? [...state[school].ids, action.payload.id]
+        : [action.payload.id];
+      return {
+        ...state,
+        [school]: {
+          ids: newIds,
+          lastMod: state[school] ? state[school].lastMod : "",
+        },
+      };
     case REMOVE_COURSE:
-      return state;
+      let schoolToRemoveFrom = "";
+      for (const school in state) {
+        if (state[school].ids.indexOf(action.payload.id) > -1) {
+          schoolToRemoveFrom = school;
+          break;
+        }
+      }
+      return {
+        ...state,
+        [schoolToRemoveFrom]: {
+          ids: state[schoolToRemoveFrom].ids.filter(
+            (id) => id !== action.payload.id
+          ),
+          lastMod: state[schoolToRemoveFrom].lastMod,
+        },
+      };
     case SAVE_TIMETABLE:
       return action.payload.idsBySchool;
     default:

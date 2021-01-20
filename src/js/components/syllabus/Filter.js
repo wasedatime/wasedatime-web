0867ad6f0cf324cae63298a5c9e12a1abdcfc93a
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-21 02:51:40 +0900

    add font; add period translation

--- src/js/components/syllabus/Filter.js
+++ src/js/components/syllabus/Filter.js
@@ -244,16 +244,16 @@ class Filter extends React.Component {
       isChecked: filterGroups[dayInputName].includes(input.value),
     }));
 
-    const periodLegend = t("syllabus.period");
+    const periodLegend = t("syllabus.period.Period");
     const periodInputName = "period";
     const periodInputs = [
-      { value: "1", label: "1" },
-      { value: "2", label: "2" },
-      { value: "3", label: "3" },
-      { value: "4", label: "4" },
-      { value: "5", label: "5" },
-      { value: "6", label: "6" },
-      { value: "7", label: "7" },
+      { value: "1", label: t("syllabus.period.1") },
+      { value: "2", label: t("syllabus.period.2") },
+      { value: "3", label: t("syllabus.period.3") },
+      { value: "4", label: t("syllabus.period.4") },
+      { value: "5", label: t("syllabus.period.5") },
+      { value: "6", label: t("syllabus.period.6") },
+      { value: "7", label: t("syllabus.period.7") },
     ];
     const checkedPeriodInputs = periodInputs.map((input) => ({
       ...input,

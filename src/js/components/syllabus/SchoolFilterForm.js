8e24787306141addef185392408a418c5e50b2f2
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-21 14:14:18 +0900

    add !important

--- src/js/components/syllabus/SchoolFilterForm.js
+++ src/js/components/syllabus/SchoolFilterForm.js
@@ -69,7 +69,7 @@ const ChooseSchoolButton = styled(Button)`
 `;
 
 const RemoveSchoolButton = styled(ChooseSchoolButton)`
-  margin-left: 1rem;
+  margin-left: 1rem !important;
   i {
     color: #ce0115 !important;
   }

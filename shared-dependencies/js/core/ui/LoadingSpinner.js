debfb82070da36dae94ea83dffd2b2f4726fdbd2
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-29 11:58:53 +0900

    adjust shared-library namespace

---
+++ shared-dependencies/js/core/ui/LoadingSpinner.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+import { Wrapper } from "@bit/wasedatime.shared-library-js.core.ui.wrapper";
+import { Overlay } from "@bit/wasedatime.shared-library-js.core.ui.overlay";
+import { Logo } from "@bit/wasedatime.shared-library-js.core.ui.logo";
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const ExtendedWrapper = styled(Wrapper)`
+  flex: 1 0 0;
+`;
+
+const ExtendedOverlay = styled(Overlay)`
+  align-items: center;
+  justify-content: center;
+  padding: 25px;
+`;
+
+const AnimatedLogo = styled(Logo)`
+  height: 100px;
+  width: 100px;
+  transition: transform(0, 12vh);
+  animation: ${spin} 3s linear infinite;
+  transform-style: preserve-3d;
+`;
+
+const Description = styled("p")`
+  font-size: 2rem;
+  text-align: center;
+`;
+
+class LoadingSpinner extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // isMounted pattern to prevent setTimeOut executing after the component unmounts.
+    this._isMounted = false;
+
+    this.state = {
+      delayMessage: "",
+    };
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this._isMounted &&
+      setTimeout(() => {
+        this._isMounted &&
+          this.setState({
+            delayMessage:
+              "Slow network is detected. Please wait we're almost done 💪",
+          });
+      }, 4000);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  render() {
+    return (
+      <ExtendedWrapper>
+        <ExtendedOverlay>
+          <AnimatedLogo alt="WasedaTime logo" />
+          <Description>{this.props.message || "Loading..."}</Description>
+          <Description>{this.state.delayMessage}</Description>
+        </ExtendedOverlay>
+      </ExtendedWrapper>
+    );
+  }
+}
+
+export default LoadingSpinner;
---
+++ shared-dependencies/js/syllabus/ui/SortingOptions.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { Wrapper } from "@bit/wasedatime.shared-library-js.core.ui.wrapper";
+import { Button } from "@bit/wasedatime.shared-library-js.core.ui.button";
+
+const ExtendedRowWrapper = styled(RowWrapper)`
+  padding: 0.3em 0;
+  justify-content: space-between;
+`;
+
+const ExtendedInvisibleButton = styled(InvisibleButton)`
+  &:hover {
+    fill: #b51e36;
+    color: #b51e36;
+  }
+  &:focus {
+    fill: ${(props) => (props.isClicked ? "#b51e36;" : "#000;")};
+    color: ${(props) => (props.isClicked ? "#b51e36;" : "#000;")};
+  }
+  fill: ${(props) => (props.isClicked ? "#b51e36;" : "#000;")};
+  color: ${(props) => (props.isClicked ? "#b51e36;" : "#000;")};
+`;
+
+// Example value of sortingOptions:
+// [
+//   { value: "added_order", label: "Added order" },
+//   { value: "course_title", label: "Course title" },
+// ]
+
+const SortingOptions = ({
+  sortingOptions,
+  selectedSortingOptionValue,
+  handleChangeSortingOption,
+}) => {
+  return (
+    <ExtendedRowWrapper>
+      {sortingOptions.map((sortingOption) => {
+        const sortingOptionValue = sortingOption.value;
+        return (
+          <ExtendedInvisibleButton
+            key={sortingOptionValue}
+            isClicked={sortingOptionValue === selectedSortingOptionValue}
+            onClick={(event) => {
+              event.preventDefault();
+              handleChangeSortingOption(sortingOptionValue);
+            }}
+          >
+            {sortingOption.label}
+          </ExtendedInvisibleButton>
+        );
+      })}
+    </ExtendedRowWrapper>
+  );
+};
+
+export default SortingOptions;

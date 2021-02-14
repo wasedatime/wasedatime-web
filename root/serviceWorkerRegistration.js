90ce1d748b68684ac2dc948fdf3807cf123fe4d3
YHhaoareyou <mlapenang1998@gmail.com>
2021-02-14 23:40:16 +0900

    remove unused env var

--- root/serviceWorkerRegistration.js
+++ root/serviceWorkerRegistration.js
@@ -23,14 +23,6 @@ const isLocalhost = Boolean(
 export function register(config) {
   if ("serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return;
-    }
-
     window.addEventListener("load", () => {
       const swUrl = `/service-worker.js`;
 

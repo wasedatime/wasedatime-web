e75fe9073591707312f3e608ccb09ccdabb77859
YHhaoareyou <mlapenang1998@gmail.com>
2021-02-19 20:15:27 +0900

    Remove unused pic

--- src/js/components/aboutUs/OurMission.js
+++ src/js/components/aboutUs/OurMission.js
@@ -3,12 +3,6 @@ import styled from "styled-components";
 import { Wrapper } from "../../styled-components/Wrapper";
 import { withNamespaces } from "react-i18next";
 
-// import reviewPageImg from "../../img/aboutus/reviewpage.jpeg"
-import syllabusPageImg from "../../../img/aboutus/syllabus.png";
-import timetablePageImg from "../../../img/aboutus/timetable.png";
-import coursereviewPageImg from "../../../img/aboutus/review.png";
-import busPageImg from "../../../img/aboutus/bus.png";
-
 const Title = styled("h2")`
   width: 50%;
   font-weight: bold;
@@ -22,11 +16,6 @@ const MainParagraph = styled("p")`
   width: 90%;
   margin: 0 auto;
   font-size: 0.9em;
-`;
-
-const FeatureArew = styled("div")`
-  width: 90%;
-  margin: 10px auto;
 `;
 
 const ImageAndDescription = styled("div")`
@@ -69,28 +58,10 @@ const OurMission = ({ t }) => {
       <Title>{t("aboutus.We are on a mission")}</Title>
       <br></br>
       <MainParagraph>
-        WasedaTime has been working on supporting students' academic activities
-        since launching. Its well-tested Syllabus Search feature is favored by
-        thousands of students. More than 50% of international students are using
-        this website. We never expect this could happen and are always grateful
-        for all our users.
+        {
+          "WasedaTime has been working on supporting students' academic activities since launching. Its well-tested Syllabus Search feature is favored by thousands of students. More than 50% of international students are using this website. We never expect this could happen and are always grateful for all our users."
+        }
       </MainParagraph>
-      {/* <FeatureArew>
-                <ImageAndDescription>
-                    <Picture1 src={syllabusPageImg}></Picture1>
-                    <Description>jiaosjdioajsodijsa</Description>
-                </ImageAndDescription>
-                <ImageAndDescription>
-                    <Picture2 src={timetablePageImg}></Picture2>
-                    <Picture3 src={coursereviewPageImg}></Picture3>
-                    <Picture4 src={busPageImg}></Picture4>
-
-                </ImageAndDescription>
-
-            </FeatureArew> */}
-
-      <br></br>
-      <br></br>
     </Wrapper>
   );
 };

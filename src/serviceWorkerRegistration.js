759f7e8e147c200d389bcb7b966416177e0b738b
YHhaoareyou <mlapenang1998@gmail.com>
2021-02-20 12:30:28 +0900

    Remove explicit skipwaiting instruction

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -4,23 +4,15 @@ export function register() {
       navigator.serviceWorker
         .register(`${process.env.PUBLIC_URL}/sw.js`)
         .then(function (registration) {
-          const installingWorker = registration.installing;
-          if (installingWorker) {
-            var refreshing;
-            installingWorker.onstatechange = () => {
-              if (installingWorker.state === "installed") {
-                if (navigator.serviceWorker.controller) {
-                  const waitingWorker = registration.waiting;
-                  if (waitingWorker) {
-                    waitingWorker.postMessage({ type: "SKIP_WAITING" });
-                    if (refreshing) return;
-                    refreshing = true;
-                    window.location.reload();
-                  }
-                }
-              }
-            };
-          }
+          var refreshing;
+          navigator.serviceWorker.addEventListener(
+            "controllerchange",
+            function () {
+              if (refreshing) return;
+              refreshing = true;
+              window.location.reload();
+            }
+          );
         })
         .catch((error) => {
           console.error("Error during service worker registration:", error);

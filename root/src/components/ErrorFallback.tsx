69ef692da6f17d02b73f5b43f558f90c41ff9ed1
YHhaoareyou <mlapenang1998@gmail.com>
2021-03-07 18:36:23 +0900

    Add error fallback component

--- root/src/components/ErrorFallback.tsx
+++ root/src/components/ErrorFallback.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+
 interface Props {
   error: Error;
   resetError: () => void;
 }
 
-const ErrorFallback = ({ error, resetError }: Props) => (
+const ErrorFallback = ({ error }: Props) => (
   <div style={{ textAlign: "center", padding: "10vw 10vh" }}>
     <h2>You have encountered an error!</h2>
     <h2>エラーが発生しました！</h2>
+    <br />
     <div>{error.toString()}</div>
+    <br />
     <div style={{ textAlign: "left" }}>
       <p>
         Please send an email to bugs@wasedatime.com and describe how the bugs
@@ -18,11 +21,10 @@ const ErrorFallback = ({ error, resetError }: Props) => (
         お手数ですが、エラー発生の前に行われた操作をメールに述べ、
         bugs@wasedatime.com へ送っていただければ助かります。
       </p>
+      <br />
       <p>We appreciate your help!</p>
       <p>ご協力ありがとうございます！</p>
     </div>
-    <button onClick={resetError}>Click here to reset!</button>
   </div>
 );
-
 export default ErrorFallback;

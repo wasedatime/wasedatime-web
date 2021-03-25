79720a3c962e4a4397378af04148a1fb54a2df32
NeroArcher <59641121+NeroArcher@users.noreply.github.com>
2021-03-25 20:36:09 +0900

    Added welcome message

--- blog/src/blog/BlogIndex.tsx
+++ blog/src/blog/BlogIndex.tsx
@@ -1,12 +1,28 @@
 import * as React from "react";
-import { styled } from "@material-ui/core/styles";
-// import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import MainFeaturedPost from "./MainFeaturedPost";
 import FeaturedPost from "./FeaturedPost";
 import careerAPI from "./careerAPI";
 import { Post } from "../types/post";
+import { Overlay } from "@bit/wasedatime.core.ts.styles.overlay";
+import styled from "styled-components";
+import MediaQuery from "react-responsive";
+import { sizes } from "@bit/wasedatime.core.ts.utils.responsive-utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
+
+
+
+const ExtendedOverlay = styled(Overlay)`
+  align-items: center;
+  background-color: #fff;
+  padding: 0 20vw;
+  color: #666;
+  hr {
+    width: 80%;
+  }
+`;
 
 const MainContainer = styled(Container)({
   padding: "50px",
@@ -44,6 +60,29 @@ class BlogIndex extends React.Component<{}, IMyComponentState> {
     const { mainFeaturedPost, featuredPosts } = this.state;
     return (
       <React.Fragment>
+        <ExtendedOverlay>
+            <br />
+            <MediaQuery minWidth={sizes.desktop}>
+              {(matches) =>
+                matches ? (
+                  <h2 style={{ color: "#555", textAlign: "center" }}>
+                    <b><FontAwesomeIcon icon={faNewspaper} /> Welcome to the article block!</b>
+                  </h2>
+                ) : (
+                  <h3 style={{ color: "#555", textAlign: "center" }}>
+                    <b><FontAwesomeIcon icon={faNewspaper} /> Welcome to the article block</b>
+                  </h3>
+                )
+              }
+            </MediaQuery>
+            <hr />
+            <br />
+            <p>
+              WasedaTime has been working on supporting students' academic
+              activities since launching. Here we offer you some articles 
+              that may be helpful for your future career.
+            </p>
+        </ExtendedOverlay>
         <MainContainer maxWidth="lg">
           <main>
             {mainFeaturedPost && <MainFeaturedPost post={mainFeaturedPost} />}

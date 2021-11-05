733735031689cdcda32330a8303212b6a1bcb80c
YHhaoareyou <mlapenang1998@gmail.com>
2021-11-05 23:13:20 +0900

    fix: remove old 'blogs' folder

--- blog/src/utils/careerAPI.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL + "/feeds",
-  params: { offset: 0, limit: 10 },
-  responseType: "json"
-});

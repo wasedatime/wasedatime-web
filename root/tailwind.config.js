1bf003d0497426c7b8497df53ae03aad42e227c9
Nicholas Narmada <nichnarmada@gmail.com>
2021-12-15 22:22:59 +0900

    Added tailwind colours

--- root/tailwind.config.js
+++ root/tailwind.config.js
@@ -1,11 +1,56 @@
 module.exports = {
   purge: {
     enabled: true, // process.env.NODE_ENV === "production"
-    content: ['./src/**/*.ejs', './src/**/*.js', './src/**/*.ts', './src/**/*.tsx', './src/**/*.html'],
+    content: [
+      "./src/**/*.ejs",
+      "./src/**/*.js",
+      "./src/**/*.ts",
+      "./src/**/*.tsx",
+      "./src/**/*.html",
+    ],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'class' or 'false'
   theme: {
-    extend: {},
+    extend: {
+      textColors: (theme) => ({
+        primary: "#b51e36",
+        primaryDarker: "#8E1728",
+        light: {
+          text1: "#000000",
+          fadetext: "#9E9E9E",
+        },
+        dark: {
+          text1: "#9E9E9E",
+          text2: "#757575",
+          fadetext: "#9E9E9E",
+        },
+      }),
+      colors: (theme) => ({
+        primary: "#b51e36",
+        primaryDarker: "#8E1728",
+        light: {
+          sidebar: "#212121",
+          text1: "#000000",
+          fadetext: "#9E9E9E",
+          button: "#8E1728",
+          cell: "#ECF0F1",
+          border: "#BDBDBD",
+          bg: "#FFFFFF",
+          linetop: "#F4F6F7",
+        },
+        dark: {
+          sidebar: "#212121",
+          text1: "#9E9E9E",
+          text2: "#757575",
+          fadetext: "#9E9E9E",
+          button: "#8E1728",
+          cell: "#424242",
+          border: "#757575",
+          bg: "#212121",
+          linetop: "#424242",
+        },
+      }),
+    },
   },
   variants: {
     extend: {},

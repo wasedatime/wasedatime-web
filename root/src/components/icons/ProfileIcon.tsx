f659618d3023a7335f8cf92f59fc4d0ec3572c4f
Nicholas Narmada <nichnarmada@gmail.com>
2022-03-16 22:36:23 +0900

    feat: added user profile icon

---
+++ root/src/components/icons/ProfileIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { AccountCircle, AccountCircleOutlined } from "@mui/icons-material";
+
+export const ProfileIcon = () => <AccountCircleOutlined />;
+
+export const ProfileIconHovered = () => <AccountCircle />;

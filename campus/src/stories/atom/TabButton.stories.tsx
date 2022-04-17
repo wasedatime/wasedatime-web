c661b401dc24727ed1b1696a180ee60604e58c9c
Nicholas Narmada <nichnarmada@gmail.com>
2022-04-17 22:22:34 +0900

    feat: created tab button story

---
+++ campus/src/stories/atom/TabButton.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { TabButton, TabProps } from "@app/components/atom/TabButton";
+
+export default {
+  component: TabButton,
+  title: "Tab Button",
+} as Meta<TabProps>;
+
+const Template: Story<TabProps> = (args) => <TabButton {...args} />;
+
+export const Selected: Story<TabProps> = Template.bind({});
+Selected.args = {
+  title: "Waseda",
+  selectedValue: "Waseda",
+  tabType: "bordered",
+  tabSize: "lg",
+};

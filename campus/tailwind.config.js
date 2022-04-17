519fc49add9d5a25a4d48a2fd5deb81dd9aed1cf
Nicholas Narmada <nichnarmada@gmail.com>
2022-04-17 23:01:20 +0900

    fix: configure daisy theme to our color palette

--- campus/tailwind.config.js
+++ campus/tailwind.config.js
@@ -16,7 +16,21 @@ module.exports = {
   plugins: [daisyui],
   daisyui: {
     styled: true,
-    themes: [],
+    themes: [
+      {
+        myTheme: {
+          primary: colors.light.main,
+          secondary: colors.light.darker,
+          accent: colors.light.secondary,
+          neutral: colors.light.text1,
+          "base-100": colors.light.bgMain,
+          info: "#3ABFF8",
+          success: "#123456",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,

0af6214ef82f766720f7fe7328ab73b7ce822d95
YHhaoareyou <mlapenang1998@gmail.com>
2022-05-31 00:28:54 +0900

    fix: remove all webpack-related packages & setting

--- syllabus/src/wasedatime-syllabus.ts
+++ syllabus/src/wasedatime-syllabus.ts
@@ -1,29 +1,9 @@
 import React from "react";
 
 import ReactDOM from "react-dom";
-import singleSpaCss from "single-spa-css";
 import singleSpaReact from "single-spa-react";
 
 import Root from "@app/root.component";
-
-const cssLifecycles = singleSpaCss({
-  // required: a list of CSS URLs to load
-  // can be omitted if webpackExtractedCss is set to true, do not specify webpack extracted css files here
-  cssUrls: [],
-
-  // optional: defaults to false. This controls whether extracted CSS files from webpack
-  // will automatically be loaded. This requires using the ExposeRuntimeCssAssetsPlugin,
-  // which is documented below.
-  webpackExtractedCss: import.meta.env.PROD,
-
-  // optional: defaults to true. Indicates whether the <link> element for the CSS will be
-  // unmounted when the single-spa microfrontend is unmounted.
-  shouldUnmount: true,
-
-  // optional: defaults to 5000. The number of milliseconds to wait on the <link> to load
-  // before failing the mount lifecycle.
-  timeout: 20000,
-});
 
 const reactLifecycles = singleSpaReact({
   React,
@@ -44,28 +24,4 @@ const reactLifecycles = singleSpaReact({
   },
 });
 
-// export const { bootstrap, mount, unmount } = reactLifecycles;
-
-// Export an array of lifecycles to integrate with a framework's
-// single-spa lifecycles. The order matters.
-export const bootstrap = import.meta.env.PROD
-  ? [cssLifecycles.bootstrap, reactLifecycles.bootstrap]
-  : reactLifecycles.bootstrap;
-
-export const mount = import.meta.env.PROD
-  ? [
-      // The css lifecycles should be before your framework's mount lifecycle,
-      // to avoid a Flicker of Unstyled Content (FOUC)
-      cssLifecycles.mount,
-      reactLifecycles.mount,
-    ]
-  : reactLifecycles.mount;
-
-export const unmount = import.meta.env.PROD
-  ? [
-      // The css lifecycles should be after your framework's unmount lifecycle,
-      // to avoid a Flicker of Unstyled Content (FOUC)
-      reactLifecycles.unmount,
-      cssLifecycles.unmount,
-    ]
-  : reactLifecycles.unmount;
+export const { bootstrap, mount, unmount } = reactLifecycles;

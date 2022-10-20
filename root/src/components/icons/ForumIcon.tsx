73e0f65edc4c0e07aaa5ba55c5503c4e3e3d153a
YHhaoareyou <mlapenang1998@gmail.com>
2022-10-20 19:32:52 +0900

    Add Forum icon to navbar

---
+++ root/src/components/icons/ForumIcon.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Forum, ForumOutlined } from "@mui/icons-material";
+
+export const ForumIcon = () => <ForumOutlined fontSize="large" />;
+
+export const ForumIconHovered = () => <Forum fontSize="large" />;

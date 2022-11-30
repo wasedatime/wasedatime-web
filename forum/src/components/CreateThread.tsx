25d8685332b62fc3111cf5c70ba2dcac524b1aab
Michael Kaminski <michael_kaminski@outlook.com>
2022-11-30 20:40:21 +0900

    feat: added tags + group buttons w/ basic toggle

--- forum/src/components/CreateThread.tsx
+++ forum/src/components/CreateThread.tsx
@@ -3,6 +3,12 @@ import React, {useState} from 'react'
 const CreateThread = () => {
 
 const [isExpanded, setIsExpanded] = useState(false)
+const [expandTags, setExpandTags] = useState(false)
+const [expandGroups, setExpandGroups] = useState(false)
+
+
+// Tags and Group buttons might be best moved to their respective components but this is how I will leave it for now.
+
   return (
     isExpanded ?  (
         <div className='relative'>
@@ -11,11 +17,29 @@ const [isExpanded, setIsExpanded] = useState(false)
        
         />
         <h1 className='absolute top-0 right-2 hover:text-light-main cursor-pointer' onClick={() => setIsExpanded(false)}>X</h1>
+        <div className='absolute bottom-0 left-2 flex mb-3 gap-2 text-sm'>
+        <button className='relative border-light-main border px-4 rounded-lg hover:text-white hover:bg-light-main' onClick={() => setExpandTags(!expandTags)}>
+            {expandTags ? (
+                <div className='bg-gray-100 border-light-main border absolute h-32 w-32 top-8 left-0 rounded-lg'>
+                Text
+                </div>
+
+            ) : null }
+            Tags</button>
+        <button className='relative border-light-main border px-4 rounded-lg hover:text-white hover:bg-light-main' onClick={() => setExpandGroups(!expandGroups)}>Groups
+        {expandGroups ? (
+                <div className='bg-gray-200 border border-light-main absolute h-32 w-32 top-8 left-0 rounded-lg'>
+                Text
+                </div>
+
+            ) : null }
+        </button>
+        </div>
         </div>
   ) : (
     <div>
         <div className='cursor-pointer' onClick={() => setIsExpanded(true)}>
-        <h1 className='border-b-2 border-light-main'>Share something....</h1>
+        <h1 className='border-b-2 border-light-main'>Start a new thread</h1>
         </div>
     </div>
   )

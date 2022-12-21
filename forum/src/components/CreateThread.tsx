d9c3e54669db7016e5c117ee57805520093d34b9
Michael Kaminski <91806277+kamlnskll@users.noreply.github.com>
2022-12-21 23:40:08 +0900

    Feature/forum-create-thread-basic (#418)
    * feat: conditional create thread text area basic
    
    * feat: added tags + group buttons w/ basic toggle
    
    * feat: added submit btn (not functional)
    
    * changed default state of expanded thread input
    
    * feat: menuitems json, createthread styling
    
    * added number type for boardindex function

---
+++ forum/src/components/CreateThread.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import menuItems from '@app/constants/menuItems.json'
+
+const CreateThread = () => {
+
+const [isExpanded, setIsExpanded] = useState(false)
+const [expandTags, setExpandTags] = useState(false)
+const [expandGroups, setExpandGroups] = useState(false)
+
+
+// Tags and Group buttons might be best moved to their respective components but this is how I will leave it for now.
+
+const { boardId } = useParams()
+
+useEffect(() => {
+ setIsExpanded(false) 
+ setExpandTags(false)
+ setExpandGroups(false)
+
+}, [boardId])
+
+const findBoardIndex: number = menuItems.findIndex((item) => item.slug == boardId)
+
+  return (
+    isExpanded ?  (
+        <div className='relative'>
+        <textarea placeholder={`Share something in ${menuItems[findBoardIndex].title}...`}
+        className='border-b-2 text-start border-light-main h-36 pl-2 pb-28 w-full hover:outline-0 focus:outline-0'
+       
+        />
+        <h1 className='absolute top-0 right-2 hover:text-light-main cursor-pointer' onClick={() => setIsExpanded(false)}>X</h1>
+        <div className='absolute bottom-0 left-2 w-full flex mb-3 text-sm justify-between'>
+        <div className='my-auto'>
+        <button className='relative border-light-main border mx-4 px-4 rounded-lg hover:text-white hover:bg-light-main' onClick={() => setExpandTags(!expandTags)}>
+            {expandTags ? (
+                <div className='bg-white border-light-main border absolute h-32 w-32 top-8 left-0 rounded-lg'>
+                Text
+                </div>
+
+            ) : null }
+            Tags</button>
+        <button className='relative border-light-main border px-4 rounded-lg hover:text-white hover:bg-light-main' onClick={() => setExpandGroups(!expandGroups)}>Groups
+        {expandGroups ? (
+                <div className='bg-white border border-light-main absolute h-32 w-32 top-8 left-0 rounded-lg'>
+                Text
+                </div>
+
+            ) : null }
+        </button>
+        </div>
+        <button className='border-light-main border mx-4 px-4 py-1 rounded-lg text-white bg-light-lighter hover:bg-light-darker'>Submit Post</button>
+        </div>
+        </div>
+  ) : (
+    <div>
+        <div className='cursor-pointer' onClick={() => setIsExpanded(true)}>
+        <h1 className='border-b-2 border-light-main'>Start a new thread</h1>
+        </div>
+    </div>
+  )
+  )
+}
+
+export default CreateThread

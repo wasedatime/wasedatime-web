6cc1a39df5f84170a2ee08f405a02b2b82e6db26
Michael Kaminski <michael_kaminski@outlook.com>
2023-01-08 18:03:30 +0900

    feat: multithread page display, basic pagination

--- forum/src/components/Board.tsx
+++ forum/src/components/Board.tsx
@@ -9,20 +9,39 @@ import threads from "@app/constants/dummy/threads.json";
 const Board = () => {
   const { boardSlug } = useParams();
   const [boardId, setBoardId] = useState(0);
+  const [countFilteredThreads, setCountFilteredThreads] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const numOfThreadsPerPage = 10;
+
+
+  const indexOfLastThread = currentPage * numOfThreadsPerPage
+  const indexOfFirstThread = indexOfLastThread - numOfThreadsPerPage
+  const currentThreads = threads.slice(indexOfFirstThread, indexOfLastThread)
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber)
+  }
+
 
   useEffect(() => {
     setBoardId(boards.find((board) => board.slug === boardSlug)?.id || 0);
   }, [boardSlug]);
 
+  useEffect(() => {
+    setCountFilteredThreads(threads.filter((thread) => thread.boardId === boardId).length)
+    setCurrentPage(1)
+    // console.log(countFilteredThreads)
+  }, [boardId])
+
   return (
     <div className="max-w-2/5 w-5/6 mx-auto h-full">
       <CreateThread />
-      {threads
+      {currentThreads
         .filter((thread) => thread.boardId === boardId)
         .map((thread, i) => (
           <ThreadBlock key={i} isPreview={true} thread={thread} />
         ))}
-      <Pagination count={threads.length} />
+      <Pagination threadCount={countFilteredThreads} numOfThreadsPerPage={numOfThreadsPerPage} paginate={paginate} />
     </div>
   );
 };
--- forum/src/components/Pagination.tsx
+++ forum/src/components/Pagination.tsx
@@ -1,36 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const numOfThreadsPerPage = 10;
 
 type Props = {
-  count: number;
+  threadCount: number;
+  numOfThreadsPerPage: number;
+  paginate: any,
+ 
 }
 
 // TODO: change page number by clicking on page button or arrow
 // TODO: filter threads to display by page number
 
-const Pagination = ({ count } : Props) => {
+const Pagination = ({ threadCount, numOfThreadsPerPage, paginate} : Props) => {
+
   return (
     <div className="flex justify-center m-4">
       <ul className="inline-flex items-center -space-x-px">
         <li>
-          <a href="#" className="block p-2 ml-0 rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">
+          <h1 className="block p-2 ml-0 rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">
             <span className="sr-only">Previous</span>
             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-          </a>
+          </h1>
         </li>
         {
-          [...Array(Math.ceil(count / numOfThreadsPerPage))].map((_, pageId) => (
-            <li key={pageId + 1}>
-              <a href="#" className="px-3 py-2 rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">{pageId + 1}</a>
+          [...Array(Math.ceil(threadCount / numOfThreadsPerPage))].map((_, pageId) => (
+            <li key={pageId + 1} className='cursor-pointer' onClick={() => paginate(pageId + 1)}>
+              <h1 className="px-3 py-2 rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">{pageId + 1}</h1>
             </li>
           ))
         }
         <li>
-          <a href="#" className="block p-2 leading-tight rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">
+          <h1 className="block p-2 leading-tight rounded-full text-gray-500 hover:bg-light-main hover:text-white dark:text-gray-300 dark:hover:bg-dark-main dark:hover:text-white">
             <span className="sr-only">Next</span>
             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </a>
+          </h1>
         </li>
       </ul>
     </div>

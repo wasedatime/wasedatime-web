f741ab5e535766e1d046a103d5c006f609b87955
YH_hao_are_you <mlapenang1998@gmail.com>
2023-01-26 23:06:03 +0900

    User auth for Forum (#421)
    * feat: User login & verify auth when opening thread form and creating new thread
    
    * chore: add todo message for implementing submitting new thread API
    
    * feat: Add auth for comment form
    
    * feat: value and onChange func for new thread form

--- apps/forum/src/components/CommentForm.tsx
+++ apps/forum/src/components/CommentForm.tsx
@@ -1,13 +1,81 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import API from "@aws-amplify/api";
+import SignInModal from "@bit/wasedatime.core.ts.ui.sign-in-modal";
+import { getIdToken } from "@bit/wasedatime.core.ts.utils.user";
+import { SendIcon } from "./icons/SendIcon";
 
 const CommentForm = () => {
+  const [userToken, setUserToken] = useState("");
+  const [isSignInModalOpen, setSignInModalOpen] = useState(false);
+  const [comment, setComment] = useState("");
+
+  const handleFocusForm = async () => {
+    if (userToken?.length <= 0) {
+      const idToken = await getIdToken();
+      if (idToken?.length > 0) {
+        setUserToken(idToken);
+      } else {
+        if (document.activeElement instanceof HTMLElement) {
+          document.activeElement.blur();
+        }
+        setSignInModalOpen(true);
+      }
+    }
+  }
+
+  const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  }
+
+  const handleSubmit = async () => {
+    if (comment.length <= 0 || comment.length > 200) return;
+
+    let idToken = userToken;
+    if (idToken?.length <= 0) {
+      idToken = await getIdToken();
+      if (idToken?.length <= 0) return;
+    }
+
+    console.log(comment);
+    // TODO: Implement submitting new comment API
+    /*
+    API.post("wasedatime-dev", "/forum/blablabla", {
+      body: { data: { comment } },
+      headers: {
+        Authorization: idToken,
+      },
+    });
+    */
+
+    setComment("");
+  }
+
   return (
     <div className="flex justify-between">
       <input
         className="text-sm text-light-text3 w-full focus:text-light-text1 focus:ring border-2 mt-4 mb-2 rounded-lg px-4 py-2"
-        placeholder="Write your comment here..."
+        placeholder="Write your comment here (no more than 200 character)"
+        value={comment}
+        onChange={handleTextChange}
+        onFocus={handleFocusForm}
+        onSubmit={handleSubmit}
         required
       />
+      <span
+        className="mt-4 mb-2 p-2 cursor-pointer text-orange-400"
+        onClick={handleSubmit}
+      >
+        <SendIcon />
+      </span>
+      {
+        isSignInModalOpen && (
+          <SignInModal
+            isModalOpen={isSignInModalOpen}
+            closeModal={() => setSignInModalOpen(false)}
+          />
+        )
+      }
+      
     </div>
   );
 };
--- apps/forum/src/components/CreateThread.tsx
+++ apps/forum/src/components/CreateThread.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import API from "@aws-amplify/api";
+import { CloseIcon } from "@app/components/icons/CloseIcon";
 import boards from "@app/constants/boards.json";
+import SignInModal from "@bit/wasedatime.core.ts.ui.sign-in-modal";
+import { getIdToken } from "@bit/wasedatime.core.ts.utils.user";
 
 const CreateThread = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [expandTags, setExpandTags] = useState(false);
   const [expandGroups, setExpandGroups] = useState(false);
+  const [userToken, setUserToken] = useState("");
+  const [isSignInModalOpen, setSignInModalOpen] = useState(false);
+  const [textContent, setTextContent] = useState("");
 
   // Tags and Group buttons might be best moved to their respective components but this is how I will leave it for now.
 
@@ -17,6 +24,47 @@ const CreateThread = () => {
     setExpandGroups(false);
   }, [boardSlug]);
 
+  const handleOpenForm = async () => {
+    if (userToken?.length > 0) {
+      setIsExpanded(true);
+    } else {
+      const idToken = await getIdToken();
+      if (idToken?.length > 0) {
+        setUserToken(idToken);
+        setIsExpanded(true);
+      } else {
+        setSignInModalOpen(true);
+      }
+    }
+  }
+
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextContent(e.target.value);
+  }
+
+  const handleSubmit = async () => {
+    if (textContent.length <= 0 || textContent.length > 2000) return;
+
+    let idToken = userToken;
+    if (idToken?.length <= 0) {
+      idToken = await getIdToken();
+      if (idToken?.length <= 0) return;
+    }
+
+    console.log(textContent);
+    // TODO: Implement submitting new thread API
+    /*
+    API.post("wasedatime-dev", "/forum/blablabla", {
+      body: { data: { textContent } },
+      headers: {
+        Authorization: idToken,
+      },
+    });
+    */
+
+    setTextContent("");
+  }
+
   const findBoardIndex: number = boards.findIndex(
     (board) => board.slug == boardSlug
   );
@@ -26,12 +74,14 @@ const CreateThread = () => {
       <textarea
         placeholder={`Share something in ${boards[findBoardIndex].title}...`}
         className="border-b-2 text-start border-light-main h-36 pl-2 pb-28 w-full hover:outline-0 focus:outline-0"
+        value={textContent}
+        onChange={handleTextChange}
       />
       <h1
         className="absolute top-0 right-2 hover:text-light-main cursor-pointer"
         onClick={() => setIsExpanded(false)}
       >
-        X
+        <CloseIcon />
       </h1>
       <div className="absolute bottom-0 left-2 w-full flex mb-3 text-sm justify-between">
         <div className="my-auto">
@@ -58,16 +108,23 @@ const CreateThread = () => {
             ) : null}
           </button>
         </div>
-        <button className="border-light-main border mx-4 px-4 py-1 rounded-lg text-white bg-light-lighter hover:bg-light-darker">
+        <button
+          className="border-light-main border mx-4 px-4 py-1 rounded-lg text-white bg-light-lighter hover:bg-light-darker"
+          onClick={handleSubmit}
+        >
           Submit Post
         </button>
       </div>
     </div>
   ) : (
     <div>
-      <div className="cursor-pointer" onClick={() => setIsExpanded(true)}>
+      <div className="cursor-pointer" onClick={handleOpenForm}>
         <h1 className="border-b-2 border-light-main">Start a new thread</h1>
       </div>
+      <SignInModal
+        isModalOpen={isSignInModalOpen}
+        closeModal={() => setSignInModalOpen(false)}
+      />
     </div>
   );
 };

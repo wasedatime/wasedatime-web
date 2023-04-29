77078c9c8deca40c691277c69684c7e92e81372d
Nicholas Narmada <nichnarmada@gmail.com>
2023-04-29 12:43:52 +0900

    chore: file linting

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import path from "node:path";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { defineConfig } from "vite"
+import path from "node:path"
+import react from "@vitejs/plugin-react-swc"
+import dts from "vite-plugin-dts"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,4 +34,4 @@ export default defineConfig({
       },
     },
   },
-});
+})

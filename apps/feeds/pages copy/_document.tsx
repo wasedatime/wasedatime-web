7290f923a36f7cf87c1b09c3197608a053486ae1
YHhaoareyou <mlapenang1998@gmail.com>
2023-06-22 19:11:46 +0900

    fix: temporarily comment out error codes in Feeds

--- apps/feeds/pages copy/_document.tsx
+++ apps/feeds/pages copy/_document.tsx
@@ -14,10 +14,10 @@ export default class CustomDocument extends Document {
     const sheets = new ServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      })
+    // ctx.renderPage = () =>
+    //   originalRenderPage({
+    //     enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    //   })
 
     const initialProps = await Document.getInitialProps(ctx)
     return {

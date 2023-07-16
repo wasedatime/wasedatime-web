737e027fee65ab0faaa456b6c96fbaa8c5ae37ea
Jason Park <joonbin0319@gmail.com>
2023-07-16 19:45:39 +0900

    feat: finishing migrate from branch to branch

--- apps/forum/src/root.component.tsx
+++ apps/forum/src/root.component.tsx
@@ -5,10 +5,24 @@ import App from "@app/components/App";
 import { ThemeProvider } from "@app/utils/theme-context";
 import { RecoilRoot } from "recoil";
 
+import { API } from "@aws-amplify/api";
+
 import "@app/utils/i18n";
 import "@app/styles/main.css";
 
 configAuth();
+
+const apiConfig = {
+  API: {
+    endpoints: [
+      {
+        name: "wasedatime-dev",
+        endpoint: import.meta.env.VITE_API_BASE_URL,
+      },
+    ],
+  },
+};
+API.configure(apiConfig);
 
 export default function Root(props) {
   return (

2679f6d36218774b4a35fd8e587972ca1feea648
YHhaoareyou <mlapenang1998@gmail.com>
2023-08-03 23:00:40 +0900

    debug: console log invalid url

--- syllabus/src/utils/get-school-icon-path.ts
+++ syllabus/src/utils/get-school-icon-path.ts
@@ -3,6 +3,10 @@ import schoolIconEN from "@app/constants/school-name-icon-map-en";
 import schoolIconJA from "@app/constants/school-name-icon-map-ja";
 
 const getSchoolIconPath = (school: string, lng: string): string => {
+  console.log("==========");
+  console.log(lng + " " + schoolIconJA[school] + " " + schoolIconEN[school]);
+  console.log(import.meta.url);
+  console.log("==========");
   const url = new URL(
     lng === Lang.JA ? schoolIconJA[school] : schoolIconEN[school],
     import.meta.url

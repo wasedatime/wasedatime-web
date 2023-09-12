7aa1659cf2ba4eaaa7583f1f2904009f86890cbc
Jason Park <joonbin0319@gmail.com>
2023-09-12 22:27:49 +0900

    feat: fixing feed back box

--- apps/forum/src/components/FeedBackBox.tsx
+++ apps/forum/src/components/FeedBackBox.tsx
@@ -8,17 +8,21 @@ const FeedBackBox = (props: Props) => {
     <div className="pl-2 md:pl-4 lg:pl-10">
       <h1 className="form-item-title">Feedback</h1>
       <p className="text-light-text1 dark:text-dark-text1">
-        The Forums is a brand new feature in WasedaTime.
-        <br /> We Would like to know how everyone's exprience is.
+        The Forums is a brand new feature in WasedaTime. There are many
+        unexpected bugs. Sorry about that. If something gets stuck spam refresh
+        and tell us.
+        <br /> We would like to know how everyone's exprience is.
         <br /> Whether there are any bugs or issues. Or any features you would
         like to be added.
         <br />
-        <Link
-          to="https://forms.gle/9qCxdehr8CrxNdbZ9"
+        <a
+          href="https://forms.gle/9qCxdehr8CrxNdbZ9"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-blue-600"
         >
           Please share your feedback by clicking on here!
-        </Link>
+        </a>
       </p>
     </div>
   );

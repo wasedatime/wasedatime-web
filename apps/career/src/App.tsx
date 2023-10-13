4c14b0a52389667e69fd27036bce44a55c462011
KTheAsianimeBoi <minhkhoahc2002@gmail.com>
2023-10-13 22:59:26 +0900

    Added Icon Folder

--- apps/career/src/App.tsx
+++ apps/career/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import Header from './components/common/Header'
+import HeaderWithModal from './components/common/HeaderWithModal'
 type Props = {}
 
 const App = (props: Props) => {

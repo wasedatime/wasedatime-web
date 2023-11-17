bdc0d0b039c41d190fa7617268b24b1cde86e8ad
Jason Park <93040528+JasonNotJson@users.noreply.github.com>
2023-11-17 20:55:03 +0900

    feat: adding mobile responsiveness to pre register profile (#502)
    * Feature/career base (#492)
    
    * feat: init feature career
    
    * chore: adding assets and types
    
    * feat: changes in header imports
    
    * feat: cleaning syllabus icons
    
    * feat: adding routing
    
    * feat: adding routing to hash router
    
    * feat: career page header with job list and job detail page base (#488)
    
    * Added Icon Folder
    
    * Header Initilize
    
    * feat: solving header issue
    
    * fix: fixing header styling
    
    * Add Job List and Job Card Element
    
    * Job List and Job Card
    
    * Job List and Job Card
    
    * Page Detail
    
    * Job Detail Page
    
    * Job Page Detail
    
    * feat: adding job description page
    
    * chore: small updates
    
    ---------
    
    Co-authored-by: KTheAsianimeBoi <minhkhoahc2002@gmail.com>
    
    * feat: finishing job details page (#489)
    
    * feat: career page user profile with api (#490)
    
    * feat: init user profile component
    
    * feat: created post login user profile component
    
    * feat: creating user profile input form
    
    * feat: creating dropdown list for profile creation modal
    
    * feat: struggling to deal with multiple dropdowns
    
    * feat: applying standard styling to jobdetail
    
    * feat: responsive styling
    
    * feat: successful implementation for user profile
    
    * feat: checked functionality next homework is apply button and time formatting
    
    * chore: adding styling to job card
    
    * feat: job apply functionality is set (#491)
    
    * fix: fixing job content sections list
    
    * feat: adding company job post button
    
    ---------
    
    Co-authored-by: KTheAsianimeBoi <minhkhoahc2002@gmail.com>
    
    * fix: fixing index html for career
    
    * fix: hotfix on vite configuration (#493)
    
    * feat: fixing button styling due to unknown errors in dev env
    
    * chore: changing minor styling issues
    
    * feat: career post application and agreement modal (#494)
    
    * feat: adding some padding and post api
    
    * feat: agreement modal and post functionality
    
    * feat: text size to section title of job content (#495)
    
    * chore(deps): update dependency postcss to v8.4.31 [security] (#480)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * feat: finishing up careers page (#496)
    
    * feat: finishing up careers
    
    * chore: cleaning console logs
    
    * chore: cleaning comments
    
    * chore(deps): update dependency postcss to v8.4.31 [security] (#498)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * feat: adding mobile responsiveness to profile registeration
    
    ---------
    
    Co-authored-by: KTheAsianimeBoi <minhkhoahc2002@gmail.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- apps/career/src/components/joblist/Joblist.tsx
+++ apps/career/src/components/joblist/Joblist.tsx
@@ -5,6 +5,8 @@ import { getIdToken, SignInModal } from "wasedatime-ui"
 import { useTranslation } from "react-i18next"
 import PreRegisterProfile from "./PreRegisterProfile"
 import type CareerComponentProps from "@app/types/careerComponentProps"
+import { sizes } from "wasedatime-ui"
+import MediaQuery from "react-responsive"
 
 type Props = {}
 
@@ -15,7 +17,7 @@ const Joblist: React.FC<CareerComponentProps> = ({
   isRegistered,
   setIsRegistered,
 }) => {
-  const [isSignInModalOpen, setSignInModalOpen] = useState(false)
+  const [jobCardsRendered, setJobCardsRendered] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userToken, setUserToken] = useState("")
   const { t } = useTranslation()
@@ -28,7 +30,7 @@ const Joblist: React.FC<CareerComponentProps> = ({
         setUserToken(idToken)
         setIsLoggedIn(true)
       } else {
-        setSignInModalOpen(true)
+        console.error("SOMETHING IS WRONG")
       }
     }
   }
@@ -37,48 +39,76 @@ const Joblist: React.FC<CareerComponentProps> = ({
     handleOpenForm()
   }, [])
 
+  useEffect(() => {
+    if (jobData.length > 0) {
+      setJobCardsRendered(true)
+    }
+  }, [jobData])
+
   return (
     <>
       {isLoggedIn ? (
-        <div className="grid grid-cols-12 gap-y-6 sm:gap-10 sm:gap-y-0">
-          <div className="col-span-12 sm:col-span-9">
-            <div className={`mt-14 py-6`}>
-              {jobData.map((job, index) => (
-                <JobCard
-                  key={index}
-                  job_id={job.job_id}
-                  company_logo={job.company_logo}
-                  company={job.company}
-                  title={job.title}
-                  job_description={job.job_description}
-                  location={job.location}
-                  created_at={job.created_at}
-                  type={job.type}
-                  applied={job.applied}
-                  isRegistered={isRegistered}
-                />
-              ))}
+        <>
+          {!isRegistered && (
+            <MediaQuery maxWidth={sizes.phone}>
+              {(matches) =>
+                matches && (
+                  <div className="col-span-12">
+                    <div className="mt-14 pt-6">
+                      <PreRegisterProfile
+                        isRegistered={isRegistered}
+                        profile={profile}
+                        setIsRegistered={setIsRegistered}
+                        setProfile={setProfile}
+                      />
+                    </div>
+                  </div>
+                )
+              }
+            </MediaQuery>
+          )}
+
+          <div className="grid grid-cols-12 gap-y-6 sm:gap-10 sm:gap-y-0">
+            <div className="col-span-12 sm:col-span-9">
+              <div className={`mt-14 py-6`}>
+                {jobData.map((job, index) => (
+                  <JobCard
+                    key={index}
+                    job_id={job.job_id}
+                    company_logo={job.company_logo}
+                    company={job.company}
+                    title={job.title}
+                    job_description={job.job_description}
+                    location={job.location}
+                    created_at={job.created_at}
+                    type={job.type}
+                    applied={job.applied}
+                    isRegistered={isRegistered}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="col-span-12 sm:col-span-3">
+              <div className="mt-14 pt-6">
+                {jobCardsRendered &&
+                  (isRegistered ? (
+                    <PostRegisterProfile
+                      isRegistered={isRegistered}
+                      profile={profile}
+                      setProfile={setProfile}
+                    />
+                  ) : (
+                    <PreRegisterProfile
+                      isRegistered={isRegistered}
+                      profile={profile}
+                      setIsRegistered={setIsRegistered}
+                      setProfile={setProfile}
+                    />
+                  ))}
+              </div>
             </div>
           </div>
-          <div className="col-span-12 sm:col-span-3">
-            <div className="mt-14 pt-6">
-              {isRegistered ? (
-                <PostRegisterProfile
-                  isRegistered={isRegistered}
-                  profile={profile}
-                  setProfile={setProfile}
-                />
-              ) : (
-                <PreRegisterProfile
-                  isRegistered={isRegistered}
-                  profile={profile}
-                  setIsRegistered={setIsRegistered}
-                  setProfile={setProfile}
-                />
-              )}
-            </div>
-          </div>
-        </div>
+        </>
       ) : (
         <div className="flex h-[calc(100vh-200px)] items-center justify-center">
           <p className="text-3xl font-bold">
--- apps/career/src/types/profileComponentProps.ts
+++ apps/career/src/types/profileComponentProps.ts
@@ -2,7 +2,7 @@ import UserProfile from "./userProfile"
 
 export interface ProfileComponentProps {
   profile: UserProfile
-  setProfile: React.Dispatch<React.SetStateAction<UserProfile>>
+  setProfile?: React.Dispatch<React.SetStateAction<UserProfile>>
   isRegistered?: boolean
   setIsRegistered?: React.Dispatch<React.SetStateAction<boolean>>
 }
